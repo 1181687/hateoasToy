8055f354ec3fdcd9ac05fd21a17058d3d1bc9a12
Luís Almeida <1181741@isep.ipp.pt>
2018-11-27 10:39:12 +0000

    Evolução do método EQUALS da Classe Sensor.

--- src/main/java/Sprint_0/Sensor.java
+++ src/main/java/Sprint_0/Sensor.java
@@ -34,15 +34,15 @@ public class Sensor {
         return mLocalizacao;
     }
 
-    public boolean equals(Object o) {
-        if (this == o) {
+    public boolean equals(Object objeto) {
+        if (this == objeto) {
             return true;
         }
-        if (!(o instanceof Sensor)) {
+        if (!(objeto instanceof Sensor)) {
             return false;
         }
-        Sensor c = (Sensor) o;
-        if (this.getmNomeSensor().equals(c.getmNomeSensor())) {
+        Sensor sensor = (Sensor) objeto;
+        if (this.mNomeSensor.equals(sensor.mNomeSensor) && this.mTipoSensor.equals(sensor.mTipoSensor) && this.mLocalizacao.equals(sensor.mLocalizacao)) {
             return true;
         } else {
             return false;
