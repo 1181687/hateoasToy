90c02262cf44197cfb6d91e3efe8f8c511161caf
Ricardo Araújo <1181726@isep.ipp.pt>
2018-12-05 12:45:37 +0000

    Testes do método listaDeRegistosEVazia.

--- src/main/java/sprint0/Sensor.java
+++ src/main/java/sprint0/Sensor.java
@@ -118,10 +118,7 @@ public class Sensor {
     }
 
     public boolean listaDeRegistosEVazia(){
-        if(mRegistos.size()==0){
-            return true;
-        }
-        return false;
+        return mRegistos.isEmpty();
     }
 
     public Medicao getUltimoRegisto() {
