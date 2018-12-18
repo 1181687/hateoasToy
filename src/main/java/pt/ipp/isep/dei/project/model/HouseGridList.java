1ad24a85531776ff2527fa40c0e5ab0ae4ca679d
Luís de Almeida <1181714@isep.ipp.pt>
2018-12-18 11:25:39 +0000

    Addresses #24 - US147 - Changes to the constructor of the classes RoomList and HouseGridList.

--- src/main/java/pt/ipp/isep/dei/project/model/HouseGridList.java
+++ src/main/java/pt/ipp/isep/dei/project/model/HouseGridList.java
@@ -1,12 +1,12 @@
 package pt.ipp.isep.dei.project.model;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class HouseGridList {
-    private List<HouseGrid> mList;
+    private List<HouseGrid> mList = new ArrayList<>();
 
-    public HouseGridList(List<HouseGrid> mList) {
-        this.mList = mList;
+    public HouseGridList() {
     }
 
     public List<HouseGrid> getmList() {
--- src/main/java/pt/ipp/isep/dei/project/model/RoomList.java
+++ src/main/java/pt/ipp/isep/dei/project/model/RoomList.java
@@ -1,12 +1,12 @@
 package pt.ipp.isep.dei.project.model;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class RoomList {
-    private List<Room> mList;
+    private List<Room> mList = new ArrayList<>();
 
-    public RoomList(List<Room> mList) {
-        this.mList = mList;
+    public RoomList() {
     }
 
     public List<Room> getmList() {
