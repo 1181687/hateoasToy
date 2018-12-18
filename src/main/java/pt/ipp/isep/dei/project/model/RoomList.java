f94a78f8e84cd1d5c02a740d312f62532e93ab92
Gabriela Alves <1181707@isep.ipp.pt>
2018-12-18 12:52:34 +0000

    Adresses #31 method addRoomToRoomList class RoomList

--- src/main/java/pt/ipp/isep/dei/project/model/RoomList.java
+++ src/main/java/pt/ipp/isep/dei/project/model/RoomList.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class RoomList {
+
     private List<Room> mList = new ArrayList<>();
 
     public RoomList() {
@@ -22,5 +23,18 @@ public class RoomList {
      */
     public Room getRoomFromASpecificPositionInTheList(int position) {
         return mList.get(position);
+    }
+
+    /**
+     * Method that adds a new room to the list of rooms
+     * @param room the new room that i want to add
+     * @return true if it adds, false if it doesn't add, because it already contains it
+     */
+    public boolean addRoomToRoomList(Room room) {
+        if (!(mList.contains(room))) {
+            mList.add(room);
+            return true;
+        }
+        return false;
     }
 }
