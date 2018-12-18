a0724231db19b85cf4dbe2a0fa5c2749cdc633f6
Gabriela Alves <1181707@isep.ipp.pt>
2018-12-18 11:04:59 +0000

    Adresses #31 Class Room with atributes, constructor and equals Method. Class Dimensions with atributes and constructor.

--- src/main/java/pt/ipp/isep/dei/project/model/Room.java
+++ src/main/java/pt/ipp/isep/dei/project/model/Room.java
@@ -1,4 +1,39 @@
 package pt.ipp.isep.dei.project.model;
 
 public class Room {
+    private String mName;
+    private int mHouseFloor;
+    private Dimensions mDimensions;
+    private HouseGrid mHouseGrid;
+
+    public Room(String mName, int mHouseFloor, Dimensions dimensions, HouseGrid mHouseGrid) {
+        this.mName = mName;
+        this.mHouseFloor = mHouseFloor;
+        this.mDimensions = dimensions;
+        this.mHouseGrid = mHouseGrid;
+    }
+
+    @Override
+    public int hashCode() {
+        return 1;
+    }
+
+    /**
+     * Equals method to determine if two Rooms are equal.
+     *
+     * @param obj receives an object
+     * @return boolean
+     */
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (!(obj instanceof Room)) {
+            return false;
+        }
+        Room roomOne = (Room) obj;
+        return this.mName.equals(roomOne.mName) && this.mHouseFloor == roomOne.mHouseFloor
+                && this.mDimensions.equals(roomOne.mDimensions) && this.mHouseGrid.equals(roomOne.mHouseGrid);
+    }
 }
