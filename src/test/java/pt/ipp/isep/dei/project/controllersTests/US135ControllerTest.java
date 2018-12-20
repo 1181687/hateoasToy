274fe9d246cb516f727d198b572126201e6ab8e9
Sónia Silva <1181729@isep.ipp.pt>
2018-12-20 16:14:05 +0000

    Adresses #29: Adding tests to US135Controller

--- src/test/java/pt/ipp/isep/dei/project/controllersTests/US135ControllerTest.java
+++ src/test/java/pt/ipp/isep/dei/project/controllersTests/US135ControllerTest.java
@@ -1,12 +1,56 @@
 package pt.ipp.isep.dei.project.controllersTests;
 
-
+import org.junit.jupiter.api.Test;
+import pt.ipp.isep.dei.project.controllers.US135Controller;
+import pt.ipp.isep.dei.project.model.HouseGrid;
 import pt.ipp.isep.dei.project.model.HouseGridList;
+import pt.ipp.isep.dei.project.model.PowerSource;
+import pt.ipp.isep.dei.project.model.PowerSourceType;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class US135ControllerTest {
 
+    @Test
+    void TestCheckIfHouseGridListIsEmptyPositive () {
 
+        // Arrange
+        HouseGridList houseGridList = new HouseGridList();
 
+        PowerSourceType powerSourceType = new PowerSourceType("public electric grid");
+        boolean powerSourceTypeIsRechargeable = false;
+
+        PowerSource powerSource = new PowerSource(powerSourceType, powerSourceTypeIsRechargeable);
+
+        US135Controller us135Controller = new US135Controller(houseGridList, powerSource);
+
+        // Act
+        boolean result = us135Controller.checkIfHouseGridListIsEmpty();
+
+        // Assert
+        assertTrue(result);
+    }
+
+    @Test
+    void TestCheckIfHouseGridListIsEmptyNegative () {
+
+        // Arrange
+        String houseGridName = "hgname1";
+        HouseGrid houseGrid1 = new HouseGrid(houseGridName);
+        HouseGridList houseGridList = new HouseGridList();
+        houseGridList.getmList().add(houseGrid1);
+
+        PowerSourceType powerSourceType = new PowerSourceType("public electric grid");
+        boolean powerSourceTypeIsRechargeable = false;
+
+        PowerSource powerSource = new PowerSource(powerSourceType, powerSourceTypeIsRechargeable);
+
+        US135Controller us135Controller = new US135Controller(houseGridList, powerSource);
+
+        // Act
+        boolean result = us135Controller.checkIfHouseGridListIsEmpty();
+
+        // Assert
+        assertFalse(result);
+    }
 }
