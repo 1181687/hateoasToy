fce56657fe49f52c9cdac2522f2875b3e729fb11
Gabriela Alves <1181707@isep.ipp.pt>
2018-12-21 12:26:16 +0000

    Adresses #31 creation of class InputValidator with methods to validate inputs introduced by the user

---
+++ src/main/java/pt/ipp/isep/dei/project/utils/InputValidator.java
@@ -0,0 +1,78 @@
+package pt.ipp.isep.dei.project.utils;
+
+import java.util.Scanner;
+
+public class InputValidator {
+
+    public static int getInt( String label){
+        Scanner in = new Scanner(System.in);
+        int x = 0;
+        boolean flag;
+        do {
+            System.out.println(label);
+            try {
+                flag = false;
+                x = in.nextInt();
+            } catch (Exception e) {
+                System.out.println("Erro input!!!");
+                flag = true;
+                in.next();
+            }
+        }while(flag);
+        return x;
+    }
+    public static int getIntPos( String label){
+        Scanner in = new Scanner(System.in);
+        int x = -1;
+        boolean flag;
+        do {
+            System.out.println(label);
+            try {
+                flag = false;
+                x = in.nextInt();
+            } catch (Exception e) {
+                System.out.println("Erro input!!!");
+                flag = true;
+                in.next();
+            }
+        }while(flag || x < 0);
+        return x;
+    }
+
+    public static int getIntRange( String label, int min , int max){
+        Scanner in = new Scanner(System.in);
+        int x = -1;
+        boolean flag;
+        do {
+            System.out.println(label);
+            try {
+                flag = false;
+                x = in.nextInt();
+            } catch (Exception e) {
+                System.out.println("Erro input!!!");
+                flag = true;
+                in.next();
+            }
+        }while(flag || x < min || x > max);
+
+        return x;
+    }
+
+    public static String getString( String label) {
+        Scanner in = new Scanner(System.in);
+        String x="";
+        boolean flag;
+        do {
+            System.out.println(label);
+            try {
+                flag = false;
+                x = in.nextLine();
+            } catch (Exception e) {
+                System.out.println("Erro input!!!");
+                flag = true;
+                in.next();
+            }
+        } while (flag);
+        return x;
+    }
+}
--- src/main/java/pt/ipp/isep/dei/project/utils/Utils.java
+++
@@ -1,4 +0,0 @@
-package pt.ipp.isep.dei.project.utils;
-
-public class Utils {
-}
