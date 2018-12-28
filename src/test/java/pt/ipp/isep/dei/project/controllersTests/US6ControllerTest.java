339713e753d09be60fbc85e99c205a55f0288925
Sónia Silva <1181729@isep.ipp.pt>
2018-12-28 11:44:22 +0000

    Solving more conflits

--- src/test/java/pt/ipp/isep/dei/project/controllersTests/US6ControllerTest.java
+++ src/test/java/pt/ipp/isep/dei/project/controllersTests/US6ControllerTest.java
@@ -4,9 +4,7 @@ import org.junit.jupiter.api.Test;
 import pt.ipp.isep.dei.project.controllers.US6Controller;
 import pt.ipp.isep.dei.project.model.*;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
 
 class US6ControllerTest {
 
