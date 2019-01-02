96eb15bb48940f30472e49365c7812f25441a44d
Sónia Silva <1181729@isep.ipp.pt>
2019-01-02 15:00:03 +0000

    Class RectangleArea: more tests

--- src/test/java/pt/ipp/isep/dei/project/modelTests/RectangleAreaTest.java
+++ src/test/java/pt/ipp/isep/dei/project/modelTests/RectangleAreaTest.java
@@ -4,34 +4,48 @@ import org.junit.jupiter.api.Test;
 import pt.ipp.isep.dei.project.model.Location;
 import pt.ipp.isep.dei.project.model.RectangleArea;
 
+import java.util.Objects;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class RectangleAreaTest {
 
     @Test
-    public void testarEqualsSame() {
-        //arrange
+    void testhashCode() {
+        //Arrange
         Location local1 = new Location(41.1496, -8.6109, 97);
-        RectangleArea area1 = new RectangleArea(10, 10, local1);
+        RectangleArea area = new RectangleArea(10, 10, local1);
 
-        boolean expectedResult = true;
-        //act
-        boolean result = area1.equals(area1);
+        int expectedResult = Objects.hash();
+
+        // Act
+        int result = area.hashCode();
+        // Assert
         assertEquals(expectedResult, result);
     }
 
     @Test
-    public void testarEqualsTrue() {
+    void testarEqualsSame() {
+        //arrange
+        Location local1 = new Location(41.1496, -8.6109, 97);
+        RectangleArea area1 = new RectangleArea(10, 10, local1);
+
+        //act
+        boolean result = area1.equals(area1);
+        assertTrue(result);
+    }
+
+    @Test
+    void testarEqualsTrue() {
         //arrange
         Location local1 = new Location(41.1496, -8.6109, 97);
         Location local2 = new Location(41.1496, -8.6109, 97);
         RectangleArea area1 = new RectangleArea(10, 10, local1);
         RectangleArea area2 = new RectangleArea(10, 10, local2);
 
-        boolean expectedResult = true;
         //act
         boolean result = area1.equals(area2);
-        assertEquals(expectedResult, result);
+        assertTrue(result);
     }
 
     @Test
@@ -42,10 +56,47 @@ class RectangleAreaTest {
         RectangleArea area1 = new RectangleArea(12, 8, local1);
         RectangleArea area2 = new RectangleArea(10, 10, local2);
 
-        boolean expectedResult = false;
         //act
         boolean result = area1.equals(area2);
-        assertEquals(expectedResult, result);
+        assertFalse(result);
+    }
+
+    @Test
+    void testarEqualsFalseDiferentValues() {
+        //arrange
+        Location local1 = new Location(-100, -200, 1);
+        Location local2 = new Location(100, 200, 100);
+        RectangleArea area1 = new RectangleArea(1, 8, local1);
+        RectangleArea area2 = new RectangleArea(5, 20, local2);
+
+        //act
+        boolean result = area1.equals(area2);
+        assertFalse(result);
+    }
+
+    @Test
+    void testarEqualsFalseMoreValues() {
+        //arrange
+        Location local1 = new Location(-200, -200, 100);
+        RectangleArea area1 = new RectangleArea(100, 80, local1);
+        RectangleArea area2 = new RectangleArea(0, 0, local1);
+
+        //act
+        boolean result = area2.equals(area1);
+        assertFalse(result);
+    }
+
+    @Test
+    public void testarEqualsFalseSameSizeArea() {
+        //arrange
+        Location local1 = new Location(-90, 180, 100);
+        Location local2 = new Location(90, -180, 0);
+        RectangleArea area1 = new RectangleArea(10, 10, local1);
+        RectangleArea area2 = new RectangleArea(10, 10, local2);
+
+        //act
+        boolean result = area1.equals(area2);
+        assertFalse(result);
     }
 
     @Test
--- src/test/java/pt/ipp/isep/dei/project/utilsTests/InputValidatorTest.java
+++ src/test/java/pt/ipp/isep/dei/project/utilsTests/InputValidatorTest.java
@@ -1,4 +1,11 @@
 package pt.ipp.isep.dei.project.utilsTests;
 
+import org.junit.jupiter.api.Test;
+import pt.ipp.isep.dei.project.utils.InputValidator;
+
+import static org.junit.jupiter.api.Assertions.*;
+
 public class InputValidatorTest {
+
+
 }
