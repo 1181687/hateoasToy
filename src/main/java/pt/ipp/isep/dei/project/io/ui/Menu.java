5a90c852a8d8883127a596999dd76f45c50ca8e2
Ricardo Araújo <1181726@isep.ipp.pt>
2019-01-10 09:03:06 +0000

    Changes to the menu Class to remove some code smells

--- src/main/java/pt/ipp/isep/dei/project/io/ui/Menu.java
+++ src/main/java/pt/ipp/isep/dei/project/io/ui/Menu.java
@@ -9,9 +9,10 @@ public class Menu {
         content.append("1-Administrator");
         content.append("\n");
         content.append("2-Regular User");
+        content.append("\n");
+        content.append("0-Exit");
         String label = content.toString();
-        int op = InputValidator.getIntRange(label, 0, 2);
-        return op;
+        return InputValidator.getIntRange(label, 0, 2);
     }
 
     public static int adminMenu() {
@@ -21,9 +22,10 @@ public class Menu {
         content.append("1-Geographical Area");
         content.append("\n");
         content.append("2-House");
+        content.append("\n");
+        content.append("0-Exit menu");
         String label = content.toString();
-        int op = InputValidator.getIntRange(label, 0, 2);
-        return op;
+        return InputValidator.getIntRange(label, 0, 2);
     }
 
     public static int adminGeoAreaMenu() {
@@ -46,10 +48,9 @@ public class Menu {
         content.append("\n");
         content.append("8-Verify if one geographical area is included in another one");
         content.append("\n");
-        content.append("0-Exit");
+        content.append("0-Exit menu");
         String label = content.toString();
-        int op = InputValidator.getIntRange(label, 0, 8);
-        return op;
+        return InputValidator.getIntRange(label, 0, 8);
     }
 
     public static int adminHouseMenu() {
@@ -64,10 +65,9 @@ public class Menu {
         content.append("\n");
         content.append("4-Add sensor to a room");
         content.append("\n");
-        content.append("0-Exit");
+        content.append("0-Exit menu");
         String label = content.toString();
-        int op = InputValidator.getIntRange(label, 0, 4);
-        return op;
+        return InputValidator.getIntRange(label, 0, 4);
     }
 
     public static int regularUserMenu() {
@@ -78,10 +78,9 @@ public class Menu {
         content.append("\n");
         content.append("2-Rooms");
         content.append("\n");
-        content.append("0-Exit");
+        content.append("0-Exit menu");
         String label = content.toString();
-        int op = InputValidator.getIntRange(label, 0, 2);
-        return op;
+        return InputValidator.getIntRange(label, 0, 2);
     }
 
     public static int regularUserHouseAreaMenu() {
@@ -94,10 +93,9 @@ public class Menu {
         content.append("\n");
         content.append("3-Average daily rainfall in the house area");
         content.append("\n");
-        content.append("0-Exit");
+        content.append("0-Exit menu");
         String label = content.toString();
-        int op = InputValidator.getIntRange(label, 0, 3);
-        return op;
+        return InputValidator.getIntRange(label, 0, 3);
     }
 
     public static int regularUserRoomsMenu() {
@@ -108,10 +106,9 @@ public class Menu {
         content.append("\n");
         content.append("2-Maximum temperature of a room in a given day");
         content.append("\n");
-        content.append("0-Exit");
+        content.append("0-Exit menu");
         String label = content.toString();
-        int op = InputValidator.getIntRange(label, 0, 2);
-        return op;
+        return InputValidator.getIntRange(label, 0, 2);
     }
 
     public static int houseGridMenu() {
@@ -126,9 +123,8 @@ public class Menu {
         content.append("\n");
         content.append("4-Detach a room from a house grid");
         content.append("\n");
-        content.append("0-Exit");
+        content.append("0-Exit menu");
         String label = content.toString();
-        int op = InputValidator.getIntRange(label, 0, 4);
-        return op;
+        return InputValidator.getIntRange(label, 0, 4);
     }
 }
