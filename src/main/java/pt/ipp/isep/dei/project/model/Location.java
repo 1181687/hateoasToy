a2f780297187ca2b2e08f5f2b6cdb715e6b87a99
Sónia Silva <1181729@isep.ipp.pt>
2019-01-11 09:54:11 +0000

    Add documentation to Location class

--- src/main/java/pt/ipp/isep/dei/project/model/Location.java
+++ src/main/java/pt/ipp/isep/dei/project/model/Location.java
@@ -1,42 +1,69 @@
 package pt.ipp.isep.dei.project.model;
 
 
-public class Location {      //graus decimais
+public class Location {
     private double mLatitude;
     private double mLongitude;
     private double mAltitude;
 
+    /**
+     * constructor of Location that receives a latitude, a longitude and an altitude.
+     * @param mLatitude
+     * @param mLongitude
+     * @param mAltitude
+     */
     public Location(double mLatitude, double mLongitude, double mAltitude) {
         setmLatitude(mLatitude);
         setmLongitude(mLongitude);
         this.mAltitude = mAltitude;
     }
 
+    /**
+     * method that get the latitude.
+     */
     public double getmLatitude() {
         return mLatitude;
     }
 
+    /**
+     * that method set the latitude.
+     * @param latitude
+     */
     public void setmLatitude(double latitude) {
         if (latitude < -90 || latitude > 90) {
             this.mLatitude = Double.NaN;
         } else this.mLatitude = latitude;
     }
 
-
+    /**
+     * method that get the longitude.
+     */
     public double getmLongitude() {
         return mLongitude;
     }
 
+    /**
+     * method that set the longitude.
+     * @param mLongitude
+     */
     public void setmLongitude(double mLongitude) {
         if (mLongitude < -180 || mLongitude > 180) {
             this.mLongitude = Double.NaN;
         } else this.mLongitude = mLongitude;
     }
 
+    /**
+     * method that get the altitude.
+     */
     public double getmAltitude() {
         return mAltitude;
     }
 
+    /**
+     * method that calculate the distance between two locations.
+     * @param novoLocal
+     * @return the distance calculated.
+     */
     public double distanceBetweenTwoLocations(Location novoLocal) {
 
         final int raioDaTerra = 6371; // raio da Terra
@@ -58,10 +85,18 @@ public class Location {      //graus decimais
         return Math.hypot(distanciaKm, altura);
     }
 
+    /**
+     * method that creates the hashcode to location
+     */
     public int hashCode() {
         return 1;
     }
 
+    /**
+     * Equals method to determine if two locations are equals.
+     * @param obj
+     * @return boolean.
+     */
     public boolean equals(Object obj) {
         if (this == obj) {
             return true;
