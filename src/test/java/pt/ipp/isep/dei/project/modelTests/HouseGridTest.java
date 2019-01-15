133a1eb017971f2a459e336fa59d033d1ee227dd
Diana Silva <1181702@isep.ipp.pt>
2019-01-15 09:59:36 +0000

    Adresses #60 corrected tests

--- src/test/java/pt/ipp/isep/dei/project/modelTests/HouseGridTest.java
+++ src/test/java/pt/ipp/isep/dei/project/modelTests/HouseGridTest.java
@@ -180,9 +180,9 @@ public class HouseGridTest {
         Dimensions dim = new Dimensions(3.5, 10.5, 20.5);
         Room room1 = new Room(name, 2, dim);
 
-        DeviceSpecs specFridge = new Fridge();
-        DeviceSpecs specWashing = new WashingMachine();
-        DeviceSpecs specDishWasher = new DishWasher();
+        DeviceSpecs specFridge = new Fridge(500,25,125,25);
+        DeviceSpecs specWashing = new WashingMachine(500,50);
+        DeviceSpecs specDishWasher = new DishWasher(500,25);
         Device dev1 = new Device("FridgeAriston", room1, specFridge);
         Device dev2 = new Device("WashingMachineBosh", room1, specWashing);
         Device dev3 = new Device("DishWasher", room1, specDishWasher);
@@ -195,7 +195,7 @@ public class HouseGridTest {
         String name2 = "KitchenBasement";
         Dimensions dim2 = new Dimensions(3.5, 30.5, 20.5);
         Room room2 = new Room(name2, -1, dim2);
-        DeviceSpecs specWaterHeater = new ElectricWaterHeater();
+        DeviceSpecs specWaterHeater = new ElectricWaterHeater(50,50,35);
         Device dev4 = new Device("FridgeSiemens", room2, specFridge);
         Device dev5 = new Device("DishWasherTeka", room2, specDishWasher);
         Device dev6 = new Device("ElectricWaterHeater", room2, specWaterHeater);
@@ -208,7 +208,7 @@ public class HouseGridTest {
         grid1.attachRoomToTheRoomList(room1);
         grid1.attachRoomToTheRoomList(room2);
 
-        double expectedResult = 1725;
+        double expectedResult = 185;
 
         //Act
         double result = grid1.getNominalPower();
