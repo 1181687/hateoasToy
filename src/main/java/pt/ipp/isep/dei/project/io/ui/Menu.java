3970aa9117185f2d15ee2f359184121a582eeef5
Sónia Silva <1181729@isep.ipp.pt>
2019-01-16 09:55:44 +0000

    Merge branch 'master' of https://bitbucket.org/switch2018/project_g3
    # Conflicts:
    #	src/main/java/pt/ipp/isep/dei/project/model/Device.java

--- src/main/java/pt/ipp/isep/dei/project/io/ui/Menu.java
+++ src/main/java/pt/ipp/isep/dei/project/io/ui/Menu.java
@@ -73,11 +73,11 @@ public class Menu {
         content.append("\n");
         content.append("8-Check the nominal power of all devices in a room");
         content.append("\n");
-        content.append("8-Add a new device to a room");
+        content.append("9-Add a new device to a room");
         content.append("\n");
         content.append("0-Return to the previous menu");
         String label = content.toString();
-        return InputValidator.getIntRange(label, 0, 8);
+        return InputValidator.getIntRange(label, 0, 9);
     }
 
     public static int regularUserMenu() {
