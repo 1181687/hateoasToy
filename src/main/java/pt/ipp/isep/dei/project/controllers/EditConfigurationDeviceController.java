2e59c7fb4a4a7b0d635cd3f810e8a3558ea493d5
Sónia Silva <1181729@isep.ipp.pt>
2019-01-16 10:25:55 +0000

    Addresses #77: Create feedback to the user in the UI

--- src/main/java/pt/ipp/isep/dei/project/controllers/EditConfigurationDeviceController.java
+++ src/main/java/pt/ipp/isep/dei/project/controllers/EditConfigurationDeviceController.java
@@ -20,8 +20,8 @@ public class EditConfigurationDeviceController {
         return this.mHouse.getRoomListContent();
     }
 
-    public RoomList getListOfRooms () {
-        return this.mHouse.getRoomList();
+    public String getRoomName (int position) {
+        return this.mHouse.getNameOfTheChosenRoomInSpecificPos(position);
     }
 
     public void getRoomByPosition (int option) {
