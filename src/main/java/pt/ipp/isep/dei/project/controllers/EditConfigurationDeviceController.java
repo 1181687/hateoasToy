f9fc76f1a21c4cb6798c98a057f4bf7d3da4cd48
Sónia Silva <1181729@isep.ipp.pt>
2019-01-21 09:49:13 +0000

    Solve code smells

--- src/main/java/pt/ipp/isep/dei/project/controllers/EditConfigurationDeviceController.java
+++ src/main/java/pt/ipp/isep/dei/project/controllers/EditConfigurationDeviceController.java
@@ -73,9 +73,8 @@ public class EditConfigurationDeviceController {
     }
 
     /**
-     * TODO
-     *
-     * @return
+     * method that get the attributes that are editable.
+     * @return the editable attributes content.
      */
     public String getEditableAttributesContent() {
         return this.mDevice.getEditableAttributesContent();
