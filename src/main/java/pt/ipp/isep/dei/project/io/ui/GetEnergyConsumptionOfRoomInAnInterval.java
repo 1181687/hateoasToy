62c04112b9bfccc42fae529211948c634faa7575
Joana Figueira <1181710@isep.ipp.pt>
2019-01-28 11:18:36 +0000

    #85 Addresses - Changes to Controller and UI

--- src/main/java/pt/ipp/isep/dei/project/io/ui/GetEnergyConsumptionOfRoomInAnInterval.java
+++ src/main/java/pt/ipp/isep/dei/project/io/ui/GetEnergyConsumptionOfRoomInAnInterval.java
@@ -1,6 +1,35 @@
 package pt.ipp.isep.dei.project.io.ui;
 
+import pt.ipp.isep.dei.project.controllers.GetEnergyConsumptionOfRoomInAnIntervalController;
+import pt.ipp.isep.dei.project.model.House;
+
+import java.time.LocalDateTime;
+
 public class GetEnergyConsumptionOfRoomInAnInterval {
+    private GetEnergyConsumptionOfRoomInAnIntervalController mController;
 
+    public GetEnergyConsumptionOfRoomInAnInterval(House house) {
+        this.mController = new GetEnergyConsumptionOfRoomInAnIntervalController(house);
+    }
 
+    public void run() {
+        if (mController.roomListIsEmpty()) {
+            System.out.println("There are no rooms in the house. Please add one.");
+            return;
+        }
+        String label1 = "Please choose a number of the list below that matches the room you want to select:\n";
+        mController.getRoomListToString();
+        int chosenRoom = InputValidator.getIntRange(label1, 0, mController.getRoomListSize()) - 1;
+        if (mController.isDeviceListEmpty(chosenRoom)) {
+            System.out.println("There are no devices in this room, so it is not possible to " +
+                    "calculate its energy consumption. Please choose another room.");
+            return;
+        }
+        String label2 = "Please insert the initial date/hour of the period you want to consider for the calculations in the following format: yyyy-MM-dd HH:mm";
+        LocalDateTime initialDate = InputValidator.getStringDateTime(label2);
+        String label3 = "Please insert the final date/hour of the period you want to consider for the calculations in the following format: yyyy-MM-dd HH:mm";
+        LocalDateTime finalDate = InputValidator.getStringDateTime(label3);
+        System.out.println("The energy consumption of room " + mController.getRoomNameByPosition(chosenRoom)
+                + " is " + mController.getEnergyConsumptionOfRoomInInterval(initialDate, finalDate));
+    }
 }
