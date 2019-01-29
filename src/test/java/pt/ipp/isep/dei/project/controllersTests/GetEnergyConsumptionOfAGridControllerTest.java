71b6c5cbfea5698bcd41f0b4363a411432c2c4a4
Diana Silva <1181702@isep.ipp.pt>
2019-01-29 12:53:06 +0000

    Adresses #86 Added tests to GetEnergyConsumptionControllerTest

--- src/test/java/pt/ipp/isep/dei/project/controllersTests/GetEnergyConsumptionOfAGridControllerTest.java
+++ src/test/java/pt/ipp/isep/dei/project/controllersTests/GetEnergyConsumptionOfAGridControllerTest.java
@@ -1,8 +1,13 @@
 package pt.ipp.isep.dei.project.controllersTests;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import pt.ipp.isep.dei.project.controllers.GetNominalPowerOfAGridController;
 import pt.ipp.isep.dei.project.model.*;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class GetEnergyConsumptionOfAGridControllerTest {
 
@@ -14,7 +19,7 @@ public class GetEnergyConsumptionOfAGridControllerTest {
         //Geographical Area
         Location location = new Location(41.178553, -8.608035, 111);
         AreaShape areaShape = new AreaShape(0.261, 0.249, location);
-        GeoAreaType geoAreaType = new GeoAreaType("Urban area");
+        GeographicalAreaType geoAreaType = new GeographicalAreaType("Urban area");
         GeographicalArea insertedGeoArea = new GeographicalArea("Campus do ISEP", geoAreaType, location, areaShape);
 
         //House
@@ -27,5 +32,45 @@ public class GetEnergyConsumptionOfAGridControllerTest {
         this.controller = new GetNominalPowerOfAGridController(houseEdificioB);
     }
 
+    @Test
+    public void checkIfGridListIsEmptyWithEmptyHouseGridListShouldReturnTrue(){
+        //Act
+        boolean result = controller.isGridListEmpty();
+        //Assert
+        assertTrue(result);
+    }
 
+    @Test
+    public void checkIfGridListIsEmptyWhenHouseGridListIsNotEmptyShouldReturnFalse(){
+        //Arrange
+        HouseGrid grid1 = new HouseGrid("Grid 1");
+        houseGridList.addHouseGrid(grid1);
+        //Act
+        boolean result = controller.isGridListEmpty();
+        //Assert
+        assertFalse(result);
+    }
+
+    @Test
+    public void getHouseGridListLengthWhenHouseGridListHasOneGridShouldReturnOne(){
+        //Arrange
+        HouseGrid grid1 = new HouseGrid("Grid 1");
+        houseGridList.addHouseGrid(grid1);
+
+        int expectedResult = 1;
+        //Act
+        int result = controller.getHouseGridListSize();
+        //Assert
+        assertEquals(expectedResult,result);
+    }
+
+    @Test
+    public void getHouseGridListLengthWhenHouseGridHasNoGridsShouldReturnZero(){
+        //Arrange
+        int expectedResult = 0;
+        //Act
+        int result = controller.getHouseGridListSize();
+        //Assert
+        assertEquals(expectedResult,result);
+    }
 }
