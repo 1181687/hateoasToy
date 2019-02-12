dcbfc98ee99f15b570819c8dd9eb5ceafdd27067
Ricardo Araújo <1181726@isep.ipp.pt>
2019-02-12 15:56:13 +0000

    Changes to Device Interface and methods.Addresses #100.

--- src/main/java/pt/ipp/isep/dei/project/model/DeviceType.java
+++ src/main/java/pt/ipp/isep/dei/project/model/DeviceType.java
@@ -2,4 +2,7 @@ package pt.ipp.isep.dei.project.model;
 
 public interface DeviceType {
     Device1 createDevice(String name, Room location);
+
+    String getTypeName();
+
 }
--- src/main/java/pt/ipp/isep/dei/project/model/ElectricWaterHeaterType.java
+++ src/main/java/pt/ipp/isep/dei/project/model/ElectricWaterHeaterType.java
@@ -1,8 +1,15 @@
 package pt.ipp.isep.dei.project.model;
 
 public class ElectricWaterHeaterType implements DeviceType {
+    private String mName;
+
     public Device1 createDevice(String name, Room location) {
         DeviceSpecs devspec = new ElectricWaterHeaterSpecs();
         return new Device1(name, location, devspec);
+    }
+
+    @Override
+    public String getTypeName() {
+        return this.mName;
     }
 }
