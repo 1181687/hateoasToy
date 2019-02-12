416c480b6c63dd8f6ecf2b59853e7a7a035d95d5
Sónia Silva <1181729@isep.ipp.pt>
2019-02-12 14:28:22 +0000

    Reduce complexity of UI's: US220 and US222

--- src/main/java/pt/ipp/isep/dei/project/io/ui/DeactivateDeviceFromRoom.java
+++ src/main/java/pt/ipp/isep/dei/project/io/ui/DeactivateDeviceFromRoom.java
@@ -12,15 +12,53 @@ public class DeactivateDeviceFromRoom {
         this.mController = new DeleteAndDeactivateDeviceFromRoomController(house);
     }
 
+    public void selectDevice() {
+        String exitMenu = "0 - Return to the previous menu";
+        String label20 = "\n> This is the list of activated devices. Please select the device you want to deativate: \n"
+                + mController.getActiveDeviceListToString() + exitMenu;
+        boolean flag20 = true;
+        while (flag20) {
+            int deviceListLength = mController.deviceListSize();
+            int position1 = InputValidator.getIntRange(label20, 0, deviceListLength) - 1;
+            if (position1 == -1) {
+                return;
+            }
+            if (mController.getDevice(position1).getIsActive()) {
+                // CONFIRM TO DEATIVATE
+                String label5 = "Are you sure do you want to deativate this device? (Y/N)";
+                String answer2 = InputValidator.confirmValidation(label5);
+                if ("y".equals(answer2) || "Y".equals(answer2)) {
+
+                    String deativateDevice = mController.deviceNameByPosition(position1);
+                    if (mController.deactivateDevice(deativateDevice)) {
+                        System.out.println("\n The device has been deativated. \n");
+                    }
+                } else {
+                    System.out.println("The device was not deactivated.");
+                    continue;
+                }
+            } else {
+                System.out.println("This device is already deactive. Please select an active device.");
+            }
+            flag20 = false;
+        }
+    }
+
+    public void showListOfDevices() {
+
+        if (this.mController.deviceListEmpty()) {
+            System.out.println("\n There are no devices in this room. \n");
+
+        } else {
+            selectDevice();
+        }
+    }
+
     public void run() {
 
         // LIST OF ROOMS
         StringBuilder content = new StringBuilder();
-
-        if (this.mController.roomListEmpty()) {
-            System.out.println("There are no rooms in the house. Please create a room.");
-
-        } else {
+        if (!this.mController.roomListEmpty()) {
             boolean flag10 = true;
             while (flag10) {
                 String exitMenu = "0 - Return to the previous menu";
@@ -33,44 +71,11 @@ public class DeactivateDeviceFromRoom {
                 mController.getRoomPosition(positionRoom);
 
                 // LIST OF DEVICES
-                if (this.mController.deviceListEmpty()) {
-                    System.out.println("\n There are no devices in this room. \n");
-
-                } else {
-
-                    String label20 = "\n> This is the list of activated devices. Please select the device you want to deativate: \n"
-                            + mController.getActiveDeviceListToString() + exitMenu;
-                    boolean flag20 = true;
-                    while (flag20) {
-                        int deviceListLength = mController.deviceListSize();
-                        int position1 = InputValidator.getIntRange(label20, 0, deviceListLength) - 1;
-                        if (position1 == -1) {
-                            return;
-                        }
-                        if (!mController.getDevice(position1).getIsActive()) {
-                            System.out.println("This device is already deactive. Please select an active device.");
-                        } else {
-
-
-                            // CONFIRM TO DEATIVATE
-                            String label5 = "Are you sure do you want to deativate this device? (Y/N)";
-                            String answer2 = InputValidator.confirmValidation(label5);
-                            if ("y".equals(answer2) || "Y".equals(answer2)) {
-
-                                String deativateDevice = mController.deviceNameByPosition(position1);
-                                if (mController.deactivateDevice(deativateDevice)) {
-                                    System.out.println("\n The device has been deativated. \n");
-                                }
-                            } else {
-                                System.out.println("The device was not deactivated.");
-                                continue;
-                            }
-                        }
-                        flag20 = false;
-                    }
-                }
+                showListOfDevices();
             }
             System.out.println(content.toString());
+        } else {
+            System.out.println("There are no rooms in the house. Please create a room.");
         }
     }
 }
