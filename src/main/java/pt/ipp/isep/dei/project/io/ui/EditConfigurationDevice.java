54b5dec97769afc98e4a9909943a0077a77cb29e
Sónia Silva <1181729@isep.ipp.pt>
2019-02-12 12:25:32 +0000

    Changes on EditConfigurationDevice UI

--- src/main/java/pt/ipp/isep/dei/project/io/ui/EditConfigurationDevice.java
+++ src/main/java/pt/ipp/isep/dei/project/io/ui/EditConfigurationDevice.java
@@ -12,104 +12,124 @@ public class EditConfigurationDevice {
         this.controller = new EditConfigurationDeviceController(house);
     }
 
+    public void case1() {
+        boolean flag = true;
+        String label4 = "What name do you want to assign to the device?";
+        String newName;
+        do {
+            newName = InputValidator.getString(label4);
+            try {
+                if (controller.setDeviceName(newName)) {
+                    System.out.println("The name was changed with success! Now, this device call " + newName + "!");
+                }
+                flag = false;
+            } catch (Exception e) {
+                System.out.println("Name already exists. Please write a new one.");
+                flag = true;
+            }
+
+        }
+        while (flag);
+    }
+
+    public void case2() {
+        String exit = "0 - Return to the previous menu";
+        String label5 = "Choose the specification you want to change.\n" + controller.getDevSpecsAttributesToString() + exit;
+        int attributePosition = InputValidator.getIntRange(label5, 0, controller.getNumberOfAttributesInDeviceSpecs());
+        if (attributePosition == 0) {
+            return;
+        }
+        String label6 = "What is the new value?";
+        double value = InputValidator.getDouble(label6);
+        if (Utils.isSameDouble(value, 0)) {
+            System.out.println("This value is not valid.");
+            return;
+        } else {
+            controller.setDeviceSpecs(attributePosition, value);
+            System.out.println("The value was changed with success!\nNow, the value is " + value + "! \n");
+            return;
+        }
+    }
+
+    public void case3() {
+        String exit = "0 - Return to the previous menu";
+        StringBuilder content = new StringBuilder();
+        String label7 = "To which room do you want to change the device?\n" + controller.getRoomListContent() + exit;
+        int roomListLength1 = controller.roomListSize();
+        int positionRoom = InputValidator.getIntRange(label7, 0, roomListLength1) - 1;
+        if (positionRoom == -1) {
+            return;
+        }
+        controller.getNewRoom(positionRoom);
+        controller.setLocation();
+        content.append("The location of the device was changed with success!\nNow, the room is ");
+        String room = controller.getRoomName(positionRoom);
+        content.append(room);
+        content.append("! \n");
+    }
+
+    public void cases() {
+        String exit = "0 - Return to the previous menu";
+        boolean flag2 = true;
+        while (flag2) {
+
+            String label3 = "What do you want to change? \n" + controller.getDeviceAttributesToString() + exit;
+            int option = InputValidator.getIntRange(label3, 0, 3);
+            if (option == 0) {
+                flag2 = false;
+                continue;
+            }
+            switch (option) {
+                case 1:
+                    this.case1();
+                    break;
+                case 2:
+                    this.case2();
+                    break;
+                case 3:
+                    this.case3();
+                    break;
+                default:
+                    System.out.println("Invalid option. Please choose a number between 1 and 3.");
+            }
+            flag2 = false;
+        }
+    }
+
     public void run() {
         StringBuilder content = new StringBuilder();
-        if (this.controller.roomListIsEmpty()) {
-            System.out.println("There are no rooms in the house. Please create a room.");
-
-        } else {
+        if (!this.controller.roomListIsEmpty()) {
             boolean flag = true;
             while (flag) {
                 String exit = "0 - Return to the previous menu";
-                String label1 = "Please select the room with the device you want to edit.";
+                String label1 = "Please select the room with the device you want to edit:\n" + controller.getRoomListContent() + exit;
                 int roomListSize = controller.roomListSize();
-                System.out.println(controller.getRoomListContent() + exit);
                 int position = InputValidator.getIntRange(label1, 0, roomListSize) - 1;
                 if (position == -1) {
                     return;
                 }
                 controller.getRoomByPosition(position);
 
-                if (this.controller.deviceListIsEmpty()) {
-                    System.out.println("There are no devices in this room.");
-
-                } else {
+                if (!this.controller.deviceListIsEmpty()) {
                     boolean flag1 = true;
                     while (flag1) {
-                        String label2 = "Please select the device you want to edit. \n" + controller.getDevicesInTheRoom() + exit;
-                        int deviceListLength = controller.getDeviceListSize();
-                        int position1 = InputValidator.getIntRange(label2, 0, deviceListLength) - 1;
+
+                        String label2 = "Please select the device you want to edit:\n" + controller.getDevicesInTheRoom() + exit;
+                        int deviceListSize = controller.getDeviceListSize();
+                        int position1 = InputValidator.getIntRange(label2, 0, deviceListSize) - 1;
                         if (position1 == -1) {
-                            flag1 = false;
-                            continue;
+                            return;
                         }
                         controller.getDeviceByPosition(position1);
 
-                        boolean flag2 = true;
-                        while (flag2) {
-                            String label3 = "What do you want to change? \n" + controller.getDeviceAttributesToString() + exit;
-                            int option = InputValidator.getIntRange(label3, 0, 3);
-                            if (option == 0) {
-                                flag2 = false;
-                                continue;
-                            }
-                            switch (option) {
-                                case 1:
-                                    String label4 = "What name do you want to assign to the device?";
-                                    String newName;
-                                    do {
-                                        newName = InputValidator.getString(label4);
-                                        try {
-                                            if (controller.setDeviceName(newName)) {
-                                                System.out.println("The name was changed with success! Now, this device call " + newName + "!");
-                                            }
-                                            flag = false;
-                                        } catch (Exception e) {
-                                            System.out.println("Name already exists. Please write a new one.");
-                                            flag = true;
-                                        }
-
-                                    }
-                                    while (flag);
-                                    break;
-                                case 2:
-                                    String label5 = "Choose the specification you want to change.\n" + controller.getDevSpecsAttributesToString() + exit;
-                                    int attributePosition = InputValidator.getIntRange(label5, 0, controller.getNumberOfAttributesInDeviceSpecs());
-                                    if (attributePosition == 0) {
-                                        continue;
-                                    }
-                                    String label6 = "What is the new value?";
-                                    double value = InputValidator.getDouble(label6);
-                                    if (Utils.isSameDouble(value, 0)) {
-                                        System.out.println("This value is not valid.");
-                                        continue;
-                                    } else {
-                                        controller.setDeviceSpecs(attributePosition, value);
-                                        System.out.println("The value was changed with success!\nNow, the value is " + value + "! \n");
-                                        break;
-                                    }
-                                case 3:
-                                    String label7 = "To which room do you want to change the device?\n" + controller.getRoomListContent() + exit;
-                                    int roomListLength1 = controller.roomListSize();
-                                    int positionRoom = InputValidator.getIntRange(label7, 0, roomListLength1) - 1;
-                                    if (positionRoom == -1) {
-                                        continue;
-                                    }
-                                    controller.getNewRoom(positionRoom);
-                                    controller.setLocation();
-                                    content.append("The location of the device was changed with success!\nNow, the room is ");
-                                    String room = controller.getRoomName(positionRoom);
-                                    content.append(room);
-                                    content.append("! \n");
-                                    break;
-                                default:
-                                    System.out.println("Invalid option. Please choose a number between 1 and 3.");
-                            }
-                            flag2 = false;
-                        }
+                        this.cases();
                     }
+                } else {
+                    System.out.println("There are no devices in this room.");
                 }
             }
+        } else {
+            System.out.println("There are no rooms in the house. Please create a room.");
         }
         System.out.println(content.toString());
     }
