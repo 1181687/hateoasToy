8f1c20a408a43650ece50443b3a46ddcd1cb6004
Ricardo Araújo <1181726@isep.ipp.pt>
2019-02-12 11:32:15 +0000

    Changes to Device Interface

--- src/main/java/pt/ipp/isep/dei/project/model/Device.java
+++ src/main/java/pt/ipp/isep/dei/project/model/Device.java
@@ -2,6 +2,23 @@ package pt.ipp.isep.dei.project.model;
 
 public interface Device {
 
+    Room getLocation();
+
+    String getName();
+
+    boolean setName();
+
+    boolean setLocation();
+
+    String getAttributesToString();
+
+    int hashCode();
+
+    boolean equals();
+
+    void setDeactivateDevice();
+
+
     String getNameToString();
 
 
