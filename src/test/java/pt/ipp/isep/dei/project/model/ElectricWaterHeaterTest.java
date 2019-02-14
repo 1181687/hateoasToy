aab1dd7d14d167d26f06698e20c7b874cd562b83
luisdealmeida <1181714@isep.ipp.pt>
2019-02-14 16:46:11 +0000

    Changes related to the Device1Test (WIP).

---
+++ src/test/java/pt/ipp/isep/dei/project/model/ElectricWaterHeaterTest.java
@@ -0,0 +1,313 @@
+package pt.ipp.isep.dei.project.model;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDateTime;
+import java.util.Map;
+import java.util.Objects;
+import java.util.TreeMap;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ElectricWaterHeaterTest {
+    private Room kitchen;
+    private Room laundry;
+    private ElectricWaterHeater electricWaterHeater;
+    private Map<LocalDateTime, Double> map;
+
+    @BeforeEach
+    public void StartUp() {
+        // Rooms
+        Dimension dim = new Dimension(3, 5, 6);
+        kitchen = new Room("Kitchen", 1, dim);
+        laundry = new Room("Laundry", 1, dim);
+
+        // Devices
+        ElectricWaterHeater dummyWaterHeater = new ElectricWaterHeater("Bosch Tronic 2000", kitchen);
+        electricWaterHeater = new ElectricWaterHeater("Bosch Tronic 3000", kitchen);
+        electricWaterHeater.setAttributesDevType("Hot-Water Temperature", 55);
+        electricWaterHeater.setAttributesDevType("Performance Ratio", 0.9);
+        electricWaterHeater.setAttributesDevType("Nominal Power", 700);
+
+        // Readings
+        LocalDateTime time0 = LocalDateTime.of(2019, 01, 24, 00, 00, 00);
+        Readings readings0 = new Readings(3, time0);
+        LocalDateTime time1 = LocalDateTime.of(2019, 01, 24, 8, 00, 00);
+        Readings readings1 = new Readings(5, time1);
+        LocalDateTime time2 = LocalDateTime.of(2019, 01, 24, 16, 00, 00);
+        Readings readings2 = new Readings(7, time2);
+        electricWaterHeater.addReadingsToTheList(readings0);
+        electricWaterHeater.addReadingsToTheList(readings1);
+        electricWaterHeater.addReadingsToTheList(readings2);
+
+        // Maps
+        map = new TreeMap<>();
+        map.put(time0, 3.0);
+        map.put(time1, 5.0);
+        map.put(time2, 7.0);
+    }
+
+    @Test
+    void getNominalPowerTest() {
+        //Arrange
+        double expectedResult = 700.0;
+
+        //Act
+        double result = electricWaterHeater.getNominalPower();
+
+        //Assert
+        assertEquals(expectedResult, result);
+    }
+
+    @Test
+    void getLocationTest() {
+        // Arrange
+        Room expectedResult = kitchen;
+
+        // Act
+        Room result = electricWaterHeater.getLocation();
+
+        // Assert
+        assertEquals(expectedResult, result);
+    }
+
+    @Test
+    void getNameTest() {
+        // Arrange
+        String expectedResult = "Bosch Tronic 3000";
+
+        // Act
+        String result = electricWaterHeater.getName();
+
+        // Assert
+        assertEquals(expectedResult, result);
+    }
+
+    @Test
+    void getTypeTest() {
+        // Arrange
+        String expectedResult = "Electric Water Heater";
+
+        // act
+        String result = electricWaterHeater.getType();
+
+        // assert
+        assertEquals(expectedResult, result);
+    }
+
+    @Test
+    public void getEnergyConsumptionInADayTest() {
+        // Arrange
+        double expectedResult = 0;
+
+        // Act
+        double result = electricWaterHeater.getEnergyConsumptionInADay();
+
+        // Assert
+        assertEquals(expectedResult, result, 0.000001);
+    }
+
+    @Test
+    public void setNameWithSameNameTest() {
+        Throwable exception = assertThrows(RuntimeException.class, () -> electricWaterHeater.setName("Bosch Tronic 3000"));
+        assertEquals("Name already exists. Please write a new one.", exception.getMessage());
+    }
+
+    @Test
+    public void setNameAlreadyInListTest() {
+        Throwable exception = assertThrows(RuntimeException.class, () -> electricWaterHeater.setName("Bosch Tronic 2000"));
+        assertEquals("Name already exists. Please write a new one.", exception.getMessage());
+    }
+
+    @Test
+    public void setNameFalseTest() {
+        // Act
+        boolean result = electricWaterHeater.setName("");
+
+        // Assert
+        assertTrue(result);
+    }
+
+    @Test
+    public void setNameTrueTest() {
+        // Act
+        boolean result = electricWaterHeater.setName("Bosch Tronic 4000");
+
+        // Assert
+        assertTrue(result);
+    }
+
+    @Test
+    void setLocationFalseTest() {
+        // Act
+        boolean result = electricWaterHeater.setLocation(kitchen);
+
+        // Assert
+        assertFalse(result);
+    }
+
+    @Test
+    void setLocationTrueTest() {
+        // Act
+        boolean result = electricWaterHeater.setLocation(laundry);
+
+        // Assert
+        assertTrue(result);
+    }
+
+    @Test
+    void getDevSpecsAttributesToStringTest() {
+        // Arrange
+        String expectedResult = "1 - Hot Water Temperature: 55.0\n" +
+                "2 - Performance Ratio: 0.9\n" +
+                "3 - Nominal Power: 700.0\n";
+        // Act
+        String result = electricWaterHeater.getDevSpecsAttributesToString();
+
+        // Assert
+        assertEquals(expectedResult, result);
+    }
+
+    @Test
+    void getAttributesToStringTest() {
+        // Arrange
+        String expectedResult = "1 - Name: Bosch Tronic 3000\n" +
+                "2 - Device1 Specifications\n" +
+                "3 - Location: Kitchen\n";
+        // Act
+        String result = electricWaterHeater.getAttributesToString();
+
+        // Assert
+        assertEquals(expectedResult, result);
+    }
+
+    @Test
+    void hashCodeTest() {
+        // Arrange
+        int expectedResult = Objects.hash(electricWaterHeater.getName());
+
+        // Act
+        int result = electricWaterHeater.hashCode();
+
+        // Assert
+        assertEquals(expectedResult, result);
+    }
+
+    @Test
+    void equalsDifferentObjectTest() {
+        // Arrange
+        Object object = new Object();
+
+        // Act
+        boolean result = electricWaterHeater.equals(object);
+
+        // Assert
+        assertFalse(result);
+    }
+
+    @Test
+    void getNumberOfSpecsAttributesTest() {
+        // Arrange
+        int expectedResult = 4;
+
+        // Act
+        int result = electricWaterHeater.getNumberOfSpecsAttributes();
+
+        // Assert
+        assertEquals(expectedResult, result);
+    }
+
+    @Test
+    void getNameToStringTest() {
+        // Arrange
+        String expectedResult = "Device: Bosch Tronic 3000, located in room: Kitchen\n";
+
+        // Act
+        String result = electricWaterHeater.getNameToString();
+
+        // Assert
+        assertEquals(expectedResult, result);
+    }
+
+    @Test
+    void getTotalEnergyConsumptionInAnIntervalWithoutSolutionsTest() {
+        // Arrange
+        double expectedResult = 0;
+
+        LocalDateTime startDate = LocalDateTime.of(2019, 01, 24, 9, 00, 00);
+        LocalDateTime endDate = LocalDateTime.of(2019, 01, 24, 15, 00, 00);
+
+        // Act
+        double result = electricWaterHeater.getEnergyConsumptionInAnInterval(startDate, endDate);
+
+        // Assert
+        assertEquals(expectedResult, result, 0.000001);
+    }
+
+    @Test
+    void getTotalEnergyConsumptionInAnIntervalWithOneSolutionTest() {
+        // Arrange
+        double expectedResult = 7;
+
+        LocalDateTime startDate = LocalDateTime.of(2019, 01, 24, 8, 00, 00);
+        LocalDateTime endDate = LocalDateTime.of(2019, 01, 24, 16, 00, 00);
+
+        // Act
+        double result = electricWaterHeater.getEnergyConsumptionInAnInterval(startDate, endDate);
+
+        // Assert
+        assertEquals(expectedResult, result, 0.000001);
+    }
+
+    @Test
+    void getTotalEnergyConsumptionInAnIntervalWithTwoSolutionsTest() {
+        // Arrange
+        double expectedResult = 12;
+
+        LocalDateTime startDate = LocalDateTime.of(2019, 01, 24, 0, 00, 00);
+        LocalDateTime endDate = LocalDateTime.of(2019, 01, 24, 16, 00, 00);
+
+        // Act
+        double result = electricWaterHeater.getEnergyConsumptionInAnInterval(startDate, endDate);
+
+        // Assert
+        assertEquals(expectedResult, result, 0.000001);
+    }
+
+    @Test
+    void getIsActiveTrueTest() {
+        // Act
+        boolean result = electricWaterHeater.getIsActive();
+
+        // Assert
+        assertTrue(result);
+    }
+
+    @Test
+    void getIsActiveFalseTest() {
+        // Assert
+        electricWaterHeater.setDeactivateDevice();
+
+        // Act
+        boolean result = electricWaterHeater.getIsActive();
+
+        // Assert
+        assertFalse(result);
+    }
+
+    @Test
+    void getDataSeriesTest() {
+        // Assert
+        LocalDateTime time0 = LocalDateTime.of(2019, 01, 24, 00, 00, 00);
+        LocalDateTime time2 = LocalDateTime.of(2019, 01, 24, 16, 00, 00);
+
+        Map<LocalDateTime, Double> expectedResult = map;
+
+        // Act
+        Map<LocalDateTime, Double> result = electricWaterHeater.getDataSeries(time0, time2);
+
+        // Assert
+        assertEquals(expectedResult, result);
+    }
+}
