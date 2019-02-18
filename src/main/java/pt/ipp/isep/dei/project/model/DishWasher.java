42046a2b19c4f97019723ff7e13fd508739272b4
Ricardo Araújo <1181726@isep.ipp.pt>
2019-02-18 16:21:57 +0000

    Addresses #43. Added more tests to remove code smells.

--- src/main/java/pt/ipp/isep/dei/project/model/DishWasher.java
+++ src/main/java/pt/ipp/isep/dei/project/model/DishWasher.java
@@ -239,7 +239,7 @@ public class DishWasher implements Device, Measurable {
     public double getEnergyConsumptionInAnInterval(LocalDateTime startDate, LocalDateTime endDate) {
         double totalEnergyConsumption = 0;
         List<Readings> readingsList = getReadingsListInInterval(startDate, endDate);
-        if (readingsList.size() > 1) {
+        if (!(readingsList.isEmpty())) {
             readingsList.remove(0);
             totalEnergyConsumption = getSumOfTheReadings(readingsList);
         }
