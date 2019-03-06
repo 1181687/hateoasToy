8198ae986dff5c596430b974f9fd62b48ec0779d
Diana Silva <1181702@isep.ipp.pt>
2019-03-06 16:02:56 +0000

    Refactor getLowestReading to getLastLowestReading Adresses #124

--- src/main/java/pt/ipp/isep/dei/project/model/geographicalarea/GeographicalArea.java
+++ src/main/java/pt/ipp/isep/dei/project/model/geographicalarea/GeographicalArea.java
@@ -434,7 +434,7 @@ public class GeographicalArea {
     public LocalDate getLastLowestMaximumReading(Location location, SensorType sensorType, LocalDate startDate, LocalDate endDate) {
         Sensor sensor = getNearestSensorWithMostRecentReading(sensorType, location);
         List<Reading> readings = sensor.getMaximumReadingsInAnInterval(startDate,endDate);
-        Reading lowestMaximumReading = sensor.getLowestReading(readings);
+        Reading lowestMaximumReading = sensor.getLastLowestReading(readings);
         return lowestMaximumReading.getDateTime().toLocalDate();
 
     }
