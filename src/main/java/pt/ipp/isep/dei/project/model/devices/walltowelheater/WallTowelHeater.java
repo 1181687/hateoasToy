c87f4f10cbff43f84ae9c3547a42b2f2338386c5
Diana Silva <1181702@isep.ipp.pt>
2019-03-11 00:25:24 +0000

    Add implementations to the WallTowerHeater class Adresses #114

--- src/main/java/pt/ipp/isep/dei/project/model/devices/walltowelheater/WallTowelHeater.java
+++ src/main/java/pt/ipp/isep/dei/project/model/devices/walltowelheater/WallTowelHeater.java
@@ -1,4 +1,56 @@
 package pt.ipp.isep.dei.project.model.devices.walltowelheater;
 
-public class WallTowelHeater {
+import pt.ipp.isep.dei.project.model.Reading;
+import pt.ipp.isep.dei.project.model.Room;
+import pt.ipp.isep.dei.project.model.devices.Device;
+import pt.ipp.isep.dei.project.model.devices.DeviceSpecs;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+public class WallTowelHeater implements Device {
+    @Override
+    public Room getLocation() {
+        return null;
+    }
+
+    @Override
+    public String getName() {
+        return null;
+    }
+
+    @Override
+    public DeviceSpecs getSpecs() {
+        return null;
+    }
+
+    @Override
+    public boolean setLocation(Room location) {
+        return false;
+    }
+
+    @Override
+    public boolean getIsActive() {
+        return false;
+    }
+
+    @Override
+    public LocalDateTime getDeactivationDate() {
+        return null;
+    }
+
+    @Override
+    public boolean setDeactivateDevice() {
+        return false;
+    }
+
+    @Override
+    public boolean setName(String name) {
+        return false;
+    }
+
+    @Override
+    public List<Reading> getReadings() {
+        return null;
+    }
 }
