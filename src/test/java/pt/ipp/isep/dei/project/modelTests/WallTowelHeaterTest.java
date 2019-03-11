7dc5cc2ed5b11980fda3991503baa34ce4f36990
Diana Silva <1181702@isep.ipp.pt>
2019-03-11 11:18:24 +0000

    Create Test class for WallTowerHeater Addresses #114

---
+++ src/test/java/pt/ipp/isep/dei/project/modelTests/WallTowelHeaterTest.java
@@ -0,0 +1,44 @@
+package pt.ipp.isep.dei.project.modelTests;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class WallTowelHeaterTest {
+
+    @Test
+    void getLocation() {
+    }
+
+    @Test
+    void getName() {
+    }
+
+    @Test
+    void getSpecs() {
+    }
+
+    @Test
+    void setLocation() {
+    }
+
+    @Test
+    void getIsActive() {
+    }
+
+    @Test
+    void getDeactivationDate() {
+    }
+
+    @Test
+    void setDeactivateDevice() {
+    }
+
+    @Test
+    void setName() {
+    }
+
+    @Test
+    void getReadings() {
+    }
+}
