571473ed5e2abd5d893dc202eaec3024e07ba800
Diana Silva <1181702@isep.ipp.pt>
2019-03-12 10:39:36 +0000

    Add BeforeEach to GetLastColdestDayHouseAreaControllerTest Addresses #124

--- src/test/java/pt/ipp/isep/dei/project/controllersTests/GetLastColdestDayHouseAreaControllerTest.java
+++ src/test/java/pt/ipp/isep/dei/project/controllersTests/GetLastColdestDayHouseAreaControllerTest.java
@@ -1,4 +1,96 @@
 package pt.ipp.isep.dei.project.controllersTests;
 
+import org.junit.jupiter.api.BeforeEach;
+import pt.ipp.isep.dei.project.controllers.getLastColdestDayHouseAreaController.GetLastColdestDayHouseAreaController;
+import pt.ipp.isep.dei.project.model.Location;
+import pt.ipp.isep.dei.project.model.Reading;
+import pt.ipp.isep.dei.project.model.geographicalarea.AreaShape;
+import pt.ipp.isep.dei.project.model.geographicalarea.GeographicalArea;
+import pt.ipp.isep.dei.project.model.geographicalarea.GeographicalAreaType;
+import pt.ipp.isep.dei.project.model.house.Address;
+import pt.ipp.isep.dei.project.model.house.House;
+import pt.ipp.isep.dei.project.model.sensor.Sensor;
+import pt.ipp.isep.dei.project.model.sensor.SensorType;
+import pt.ipp.isep.dei.project.utils.Utils;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
 public class GetLastColdestDayHouseAreaControllerTest {
+
+    private static final String CONFIG_PROPERTIES = "Configuration.properties";
+    private GeographicalArea northernRegion;
+    private GeographicalArea portoDistrict;
+    private GeographicalArea portoCity;
+    private Sensor temperatureSensor;
+    private Sensor temperatureSensor1;
+    private SensorType temperature;
+    private Location location2;
+    private House house;
+    private GetLastColdestDayHouseAreaController controller;
+
+    @BeforeEach
+    public void StartUp() {
+        // Geographical Area Types
+        GeographicalAreaType region = new GeographicalAreaType("Region");
+        GeographicalAreaType district = new GeographicalAreaType("District");
+        GeographicalAreaType city = new GeographicalAreaType("City");
+
+        // Geographical Areas
+        Location location = new Location(32.1496, 7.6109, 98);
+        AreaShape areaShape = new AreaShape(100, 100, location);
+        northernRegion = new GeographicalArea("Norte", "Northern Region", region, location, areaShape);
+        Location location1 = new Location(41.1496, -6.6109, 100);
+        AreaShape areaShape1 = new AreaShape(40, 40, location1);
+        portoDistrict = new GeographicalArea("Distrito do Porto", "Porto District", district, location1, areaShape1);
+        portoDistrict.setInsertedIn(northernRegion);
+        this.location2 = new Location(42.1496, -8.6109, 97);
+        AreaShape areaShape2 = new AreaShape(10, 10, location2);
+        portoCity = new GeographicalArea("Porto", "Porto City", city, location2, areaShape2);
+        portoCity.setInsertedIn(portoDistrict);
+
+        // House
+        int meteringPeriodGrid = Integer.parseInt(Utils.readConfigFile(CONFIG_PROPERTIES, "MeteringPeriodGrid"));
+        int meteringPeriodDevice = Integer.parseInt(Utils.readConfigFile(CONFIG_PROPERTIES, "MeteringPeriodDevice"));
+        List<String> deviceTypeList = Utils.readConfigFileToList(CONFIG_PROPERTIES, "devicetype.count", "devicetype.name");
+        this.house = new House(deviceTypeList, meteringPeriodGrid, meteringPeriodDevice);
+        Location houseLocation = new Location(41.178553, -8.608035, 111);
+        Address address = new Address("4200-072", houseLocation);
+        this.house.setAddress(address);
+        this.house.setInsertedGeoArea(portoCity);
+
+
+        // Sensors
+        temperature = new SensorType("temperature");
+        LocalDateTime startDate = LocalDateTime.of(2018, 12, 2, 15, 20, 00);
+        Location sensorLocation = new Location(38.1596, -8.6109, 97);
+        temperatureSensor = new Sensor("S01", "A123", startDate, temperature, sensorLocation, "l/m2");
+        LocalDateTime startDate1 = LocalDateTime.of(2018, 12, 5, 15, 20, 00);
+        Location sensorLocation1 = new Location(42.1496, -8.6109, 97);
+        temperatureSensor1 = new Sensor("S01", "B123", startDate1, temperature, sensorLocation1, "l/m2");
+
+        // Reading
+        LocalDateTime readingDate = LocalDateTime.of(2018, 12, 2, 13, 20, 00);
+        LocalDateTime readingDate1 = LocalDateTime.of(2018, 12, 3, 13, 24, 00);
+        Reading reading = new Reading(23, readingDate);
+        Reading reading1 = new Reading(30, readingDate1);
+        temperatureSensor.addReadingsToList(reading);
+        temperatureSensor.addReadingsToList(reading1);
+        LocalDateTime readingDate2 = LocalDateTime.of(2018, 12, 2, 05, 20, 00);
+        LocalDateTime readingDate3 = LocalDateTime.of(2018, 12, 3, 05, 24, 00);
+        Reading reading2 = new Reading(22, readingDate2);
+        Reading reading3 = new Reading(25, readingDate3);
+        temperatureSensor1.addReadingsToList(reading2);
+        temperatureSensor1.addReadingsToList(reading3);
+
+        //Add sensors to SensorList
+
+        portoCity.getSensorListInTheGeographicArea().addSensor(temperatureSensor);
+        portoCity.getSensorListInTheGeographicArea().addSensor(temperatureSensor1);
+
+        controller = new GetLastColdestDayHouseAreaController(house);
+    }
+
+
+
 }
