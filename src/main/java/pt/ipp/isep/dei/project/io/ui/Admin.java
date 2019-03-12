0718c175c027b3e8fe77c800eb6a33a4456a9890
luisdealmeida <1181714@isep.ipp.pt>
2019-03-12 13:06:32 +0000

    Fixes #125 - Changes to the Import Readings' UI (The automatic FileNotFoundException was taken out and now, when it can't reach the CSV file, an error string is souted).

--- src/main/java/pt/ipp/isep/dei/project/io/ui/Admin.java
+++ src/main/java/pt/ipp/isep/dei/project/io/ui/Admin.java
@@ -7,8 +7,6 @@ import pt.ipp.isep.dei.project.model.house.RoomList;
 import pt.ipp.isep.dei.project.model.house.powersource.PowerSourceTypeList;
 import pt.ipp.isep.dei.project.model.sensor.SensorTypeList;
 
-import java.io.FileNotFoundException;
-
 public class Admin {
     private GeographicalAreaTypeList geographicalAreaTypeList;
     private GeographicalAreaList geographicalAreaList;
@@ -69,11 +67,8 @@ public class Admin {
                     break;
                 case 10:
                     ImportReadingsFromCSV ui10 = new ImportReadingsFromCSV(geographicalAreaList);
-                    try {
-                        ui10.run();
-                    } catch (FileNotFoundException e) {
-                        System.out.println(e.getMessage() + "\n");
-                    }
+                    ui10.run();
+                    break;
             }
             option = Menu.adminGeoAreaMenu();
         }
