91eeb009194c089ec921d5c62b3b42e3094dee81
Diana Silva <1181702@isep.ipp.pt>
2019-03-12 14:24:16 +0000

    Change getHighestReadingOfADay method to return the most recent reading Adresses #124

--- src/main/java/pt/ipp/isep/dei/project/model/sensor/Sensor.java
+++ src/main/java/pt/ipp/isep/dei/project/model/sensor/Sensor.java
@@ -517,7 +517,7 @@ public class Sensor {
         }
         Reading highestReading = getDailyMeasurement(day).get(0);
         for (Reading reading : getDailyMeasurement(day)) {
-            if (!Double.isNaN(reading.getValue()) && Utils.isFirstDoubleBiggerThanSecondOne(reading.getValue(), highestReading.getValue())) {
+            if (!Double.isNaN(reading.getValue()) && Utils.isFirstDoubleBiggerOrEqualThanSecondOne(reading.getValue(), highestReading.getValue())) {
                 highestReading = reading;
             }
         }
