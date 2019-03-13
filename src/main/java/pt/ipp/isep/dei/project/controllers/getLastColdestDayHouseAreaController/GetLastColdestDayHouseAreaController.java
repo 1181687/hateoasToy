1c8f80762c02bbb3689a4803e4e583b409a05c3a
Diana Silva <1181702@isep.ipp.pt>
2019-03-13 11:03:45 +0000

    Change methods in GetLastColdestDayHouseArea controller #124

--- src/main/java/pt/ipp/isep/dei/project/controllers/getLastColdestDayHouseAreaController/GetLastColdestDayHouseAreaController.java
+++ src/main/java/pt/ipp/isep/dei/project/controllers/getLastColdestDayHouseAreaController/GetLastColdestDayHouseAreaController.java
@@ -4,6 +4,7 @@ import pt.ipp.isep.dei.project.model.Reading;
 import pt.ipp.isep.dei.project.model.ReadingDTO;
 import pt.ipp.isep.dei.project.model.ReadingMapper;
 import pt.ipp.isep.dei.project.model.house.House;
+import pt.ipp.isep.dei.project.model.sensor.Sensor;
 import pt.ipp.isep.dei.project.model.sensor.SensorType;
 
 import java.time.LocalDate;
@@ -18,7 +19,8 @@ public class GetLastColdestDayHouseAreaController {
     }
 
     public boolean hasReadingsBetweenDates(LocalDate startDate, LocalDate endDate) {
-        return house.checkMeasurementExistenceBetweenDates(this.house.getLocation(), startDate, endDate);
+        Sensor nearestSensor = house.getNearestSensorWithMostRecentReading(this.sensorTypeTemperature,house.getLocation());
+        return nearestSensor.checkMeasurementExistenceBetweenDates(startDate,endDate);
     }
 
     public ReadingDTO getLastLowestMaximumReading(LocalDate startDate, LocalDate endDate) {
