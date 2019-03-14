645b7ba8195f07343936d4139803760e2451e5d0
Sónia Silva <1181729@isep.ipp.pt>
2019-03-14 21:07:14 +0000

    #118 addresses: add test on JSONReader class

--- src/test/java/pt/ipp/isep/dei/project/utilsTests/JSONReaderTest.java
+++ src/test/java/pt/ipp/isep/dei/project/utilsTests/JSONReaderTest.java
@@ -1,18 +1,43 @@
 package pt.ipp.isep.dei.project.utilsTests;
 
+import org.junit.jupiter.api.Test;
+import pt.ipp.isep.dei.project.model.geographicalarea.GeographicalArea;
+import pt.ipp.isep.dei.project.model.geographicalarea.GeographicalAreaDTO;
+import pt.ipp.isep.dei.project.model.geographicalarea.GeographicalAreaMapping;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class JSONReaderTest {
 
-    /*
     @Test
     public void readJSONFileToList() {
-
         // arrange
-        List<GeographicalAreaDTO> geographicalAreaDTOList = new ArrayList<>();
-        List<GeographicalAreaDTO> expectedResult = geographicalAreaDTOList;
-        // act
-        List<GeographicalAreaDTO> result = JSONReader.readJSONFileToList("JSONfile.json");
+        // geo area ISEP
+        String id = "ISEP";
+        String description = "Campus do ISEP";
+        String type = "Urban area";
+        double width = 0.261;
+        double length = 0.249;
+        double latitude = 41.178553;
+        double longitude = -8.608035;
+        double altitude = 111;
+
+        GeographicalAreaDTO geographicalAreaDTO = new GeographicalAreaDTO();
+        geographicalAreaDTO.setId(id);
+        geographicalAreaDTO.setName(description);
+        geographicalAreaDTO.setType(type);
+        geographicalAreaDTO.setLength(length);
+        geographicalAreaDTO.setWidth(width);
+        geographicalAreaDTO.setLatitude(latitude);
+        geographicalAreaDTO.setLongitude(longitude);
+        geographicalAreaDTO.setAltitude(altitude);
+
+        GeographicalArea expectedResult = GeographicalAreaMapping.mapToEntity(geographicalAreaDTO);
+
+        GeographicalAreaDTO geoAreaDTO = GeographicalAreaMapping.mapToDTO(id, description, type, width, length, latitude, longitude, altitude);
+        GeographicalArea result = GeographicalAreaMapping.mapToEntity(geoAreaDTO);
+
         // assert
         assertEquals(expectedResult, result);
     }
-    */
 }
