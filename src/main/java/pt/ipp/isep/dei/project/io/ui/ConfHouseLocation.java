8ef147c6c195edf35fc617f1613f3ad1ac9d4239
luisdealmeida <1181714@isep.ipp.pt>
2019-03-14 11:38:56 +0000

    Fixes #131 - Changes to the way the location of a House is configured.

--- src/main/java/pt/ipp/isep/dei/project/io/ui/ConfHouseLocation.java
+++ src/main/java/pt/ipp/isep/dei/project/io/ui/ConfHouseLocation.java
@@ -1,7 +1,6 @@
 package pt.ipp.isep.dei.project.io.ui;
 
 import pt.ipp.isep.dei.project.controllers.ConfHouseLocationController;
-import pt.ipp.isep.dei.project.model.geographicalarea.GeographicalAreaDTO;
 import pt.ipp.isep.dei.project.model.geographicalarea.GeographicalAreaList;
 import pt.ipp.isep.dei.project.model.house.House;
 
@@ -10,10 +9,9 @@ import java.util.Scanner;
 public class ConfHouseLocation {
 
     /**
-     * US101 As an Administrator, I want to configure the location of the housegrid.
+     * US101 As an Administrator, I want to configure the location of the house.
      */
     private ConfHouseLocationController controller;
-    private GeographicalAreaDTO geographicalAreaDTO;
 
     public ConfHouseLocation(GeographicalAreaList geographicalAreaList, House house) {
         this.controller = new ConfHouseLocationController(geographicalAreaList, house);
@@ -21,6 +19,10 @@ public class ConfHouseLocation {
 
 
     public void run() {
+        if (controller.isGeographicalAreaListEmpty()) {
+            System.out.println("\nThere's no geographical areas in the system. Please create or import some.\n");
+            return;
+        }
         System.out.println("Introduce the zip code of the new location.");
         Scanner ler = new Scanner(System.in);
         String zipCode = ler.nextLine();
@@ -33,13 +35,23 @@ public class ConfHouseLocation {
         String label3 = "Introduce the altitude of the new location.";
         double altitude = InputValidator.getInt(label3);
 
-        String label4 = "Choose the number that corresponds to the geographical area you wish to include in another geographical area.";
-        int positionChosenGeoArea = InputValidator.getIntRange(label4
-                + "\n" + controller.getGeoAreaListToString(true), 1, controller.getNumberOfGeoAreas());
-        controller.setGeographicalArea(positionChosenGeoArea);
+        boolean flag = true;
+        do {
+            String label4 = "Choose the number that corresponds to the geographical area you wish to include the house in.";
+            String list = controller.getGeoAreaListToString(true);
+            int positionChosenGeoArea = InputValidator.getIntRange(label4
+                    + "\n" + list, 1, controller.getNumberOfGeoAreas()) - 1;
+            controller.setGeographicalArea(positionChosenGeoArea);
 
-        controller.defineNewAddress(zipCode, latitude, longitude, altitude);
-        controller.setAddress();
+            if (!controller.isHouseAlreadyInGeoArea()) {
+                controller.defineNewAddress(zipCode, latitude, longitude, altitude);
+                controller.setAddress();
+                flag = false;
+            } else {
+                System.out.println("\nThe house is already in the chosen geographical area. Please choose another one.\n");
+                continue;
+            }
+        } while (flag);
         System.out.println("\nSuccess!\n");
     }
 }
