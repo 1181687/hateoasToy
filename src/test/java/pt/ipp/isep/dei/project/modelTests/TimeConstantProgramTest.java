01dccb2acac038f476ecf5f62c32701c9837d0dd
Gabriela Xavier <1181687@isep.ipp.pt>
2019-03-14 00:02:23 +0000

    Part of TimeConstantProgramTest.

--- src/test/java/pt/ipp/isep/dei/project/modelTests/TimeConstantProgramTest.java
+++ src/test/java/pt/ipp/isep/dei/project/modelTests/TimeConstantProgramTest.java
@@ -1,5 +1,44 @@
 package pt.ipp.isep.dei.project.modelTests;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import pt.ipp.isep.dei.project.model.devices.ProgramSpecs;
+import pt.ipp.isep.dei.project.model.devices.TimeConstantProgram;
+import pt.ipp.isep.dei.project.model.devices.TimeConstantProgramSpecs;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class TimeConstantProgramTest {
+
+    @Test
+    public void testGetProgramSpecs (){
+        //Arrange
+        String name = "prog1";
+        TimeConstantProgramSpecs specs= new TimeConstantProgramSpecs();
+        TimeConstantProgram timeConstantProgram = new TimeConstantProgram(name, specs);
+
+        ProgramSpecs expectedResult= specs;
+
+        //Act
+        ProgramSpecs result = timeConstantProgram.getProgramSpecs();
+
+        //Assert
+        assertEquals(expectedResult, result);
+    }
+
+    @Test
+    public void testSetProgramAttributes_duration (){
+        //Arrange
+        String name = "prog1";
+        TimeConstantProgramSpecs specs= new TimeConstantProgramSpecs();
+        TimeConstantProgram timeConstantProgram = new TimeConstantProgram(name, specs);
+
+        //Act
+
+
+        //Assert
+    }
+
+
 
 }
