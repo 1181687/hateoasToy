a0e0ade8432005d508b58404ce9c047dbc1eb06e
Joana Figueira <1181710@isep.ipp.pt>
2019-03-20 17:02:52 +0000

    Addresses #136 - Added dependencies to pom.xml related to Jackson. Started creation of jsonReader_v2 in order to created a general reader. Changed CSVReader (it is no longer a static class).

--- src/test/java/pt/ipp/isep/dei/project/utilsTests/CSVReaderTest.java
+++ src/test/java/pt/ipp/isep/dei/project/utilsTests/CSVReaderTest.java
@@ -1,29 +1,30 @@
 package pt.ipp.isep.dei.project.utilsTests;
 
 class CSVReaderTest {
-    /*
+
     /**
      * Test that tries to parse a valid String, which results in a list with the required information.
-     /
-    @Test
+     */
+    /*@Test
     void testParseLine_tryingToParseAValidString_ShouldReturnSuccessfulResults() {
         // Arrange
         String line = "TT12346,2018-12-30T02:00:00+00:00,14.0";
 
         // Act
-        List<String> result = CSVReader.parseLine(line);
+        List<String> result = parseLine(line);
 
         // Assert
+        assert
         assertEquals(3, result.size());
         assertEquals("TT12346", result.get(0));
         assertEquals("2018-12-30T02:00:00+00:00", result.get(1));
         assertEquals("14.0", result.get(2));
-    }
+    }*/
 
     /**
      * Test that tries to parse an empty String, which results in a null Object.
-     /
-    @Test
+     */
+    /*@Test
     void testParseLine_tryingToParseAnEmptyString_ShouldReturnWithASizeOfZero() {
         // Arrange
         String line = "";
@@ -33,12 +34,12 @@ class CSVReaderTest {
 
         // Assert
         assertEquals(0, result.size());
-    }
+    }*/
 
     /**
      * Test that tries to parse a null String, which results in a null Object.
-     /
-    @Test
+     */
+    /*@Test
     void testParseLine_tryingToParseANullString_ShouldReturnWithASizeOfZero() {
         // Arrange
         String line = null;
