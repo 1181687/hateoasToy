fea9d3dd2f4b9b094ea6d1b88aedb4b4a53a659d
luisdealmeida <1181714@isep.ipp.pt>
2019-03-22 19:25:45 +0000

    Revert "Lombok usage added (experimenting on LocationDTO)."
    This reverts commit 8ddd8d64

--- src/main/java/pt/ipp/isep/dei/project/model/LocationDTO.java
+++ src/main/java/pt/ipp/isep/dei/project/model/LocationDTO.java
@@ -1,16 +1,38 @@
 package pt.ipp.isep.dei.project.model;
 
-import lombok.EqualsAndHashCode;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-
-@EqualsAndHashCode
-@Getter
-@Setter
-@NoArgsConstructor
 public class LocationDTO {
     private double latitude;
     private double longitude;
     private double elevation;
+
+    /**
+     * constructor of Location that receives a latitude, a longitude and an elevation.
+     */
+    public LocationDTO() {
+        //Intentionally empty
+    }
+
+    public double getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(double latitude) {
+        this.latitude = latitude;
+    }
+
+    public double getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(double longitude) {
+        this.longitude = longitude;
+    }
+
+    public double getElevation() {
+        return elevation;
+    }
+
+    public void setElevation(double elevation) {
+        this.elevation = elevation;
+    }
 }
