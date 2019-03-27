5a78ffc579bc0d381ab2b3891cb513b0077bc1fc
Sónia Silva <1181729@isep.ipp.pt>
2019-03-27 10:06:10 +0000

    addresses #132: Elaboration of couplings related to the QA function

--- src/main/java/pt/ipp/isep/dei/project/model/FileReader.java
+++ src/main/java/pt/ipp/isep/dei/project/model/FileReader.java
@@ -1,12 +1,13 @@
 package pt.ipp.isep.dei.project.model;
 
 import java.util.List;
+import java.util.Objects;
 
 public interface FileReader {
 
 
     String getTypeName();
 
-    List<List<String>> readFile();
+    List<Objects> readFile();
 
 }
