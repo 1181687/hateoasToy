0ba7abef95d8a54393d3ea41d66ddee9862aa828
Gabriela Alves <1181707@isep.ipp.pt>
2019-04-11 12:33:52 +0100

    fixes #170 - [Related to US061] - import Readings GeoArea sensors without duplication.

--- src/main/java/pt/ipp/isep/dei/project/model/sensor/GeoAreaSensor.java
+++ src/main/java/pt/ipp/isep/dei/project/model/sensor/GeoAreaSensor.java
@@ -303,8 +303,17 @@ public class GeoAreaSensor implements Sensor {
 
 
     public boolean addReading(Reading reading) {
-        if (!listOfReadings.contains(reading)) {
+        if (!this.readingExistsBySensorIdLocalDateTime(reading)) {
             return this.listOfReadings.add(reading);
+        }
+        return false;
+    }
+
+    public boolean readingExistsBySensorIdLocalDateTime(Reading reading) {
+
+        for (Reading reading1 : listOfReadings) {
+            if (reading1.getDateTime() == reading.getDateTime())
+                return true;
         }
         return false;
     }
