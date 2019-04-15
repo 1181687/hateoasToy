6058de43dacf6c98a195113b7e668360490cadd6
Diana Ferreira <1181703@isep.ipp.pt>
2019-04-15 15:04:42 +0100

    Changes to GeoAreaType, PowerSourceType and SensorType Repositories (receives object Id instead of String)

--- src/main/java/pt/ipp/isep/dei/project/GeoAreaTypeRepository.java
+++ src/main/java/pt/ipp/isep/dei/project/GeoAreaTypeRepository.java
@@ -1,7 +1,8 @@
 package pt.ipp.isep.dei.project;
 
 import org.springframework.data.repository.CrudRepository;
+import pt.ipp.isep.dei.project.model.geographicalarea.GeoAreaTypeId;
 import pt.ipp.isep.dei.project.model.geographicalarea.GeographicalAreaType;
 
-public interface GeoAreaTypeRepository extends CrudRepository<GeographicalAreaType,String> {
+public interface GeoAreaTypeRepository extends CrudRepository<GeographicalAreaType, GeoAreaTypeId> {
 }
