9e1728aab6905c1b864433b58ec5d6642e5c94d4
Diana Ferreira <1181703@isep.ipp.pt>
2019-04-15 15:47:02 +0100

    commented methods

--- src/main/java/pt/ipp/isep/dei/project/model/sensor/RoomSensorMapper.java
+++ src/main/java/pt/ipp/isep/dei/project/model/sensor/RoomSensorMapper.java
@@ -9,10 +9,13 @@ public final class RoomSensorMapper {
     public static RoomSensorDTO newRoomSensorDTO() {
         return new RoomSensorDTO();
     }
+}
 
-    public static RoomSensor mapToEntity(RoomSensorDTO sensorDTO) {
+    /*public static RoomSensor mapToEntity(RoomSensorDTO sensorDTO) {
         SensorType type = new SensorType(sensorDTO.getSensorType());
         SensorTypeId typeId = new SensorTypeId(type.getSensorType());
         return new RoomSensor(sensorDTO.getId(), sensorDTO.getName(), sensorDTO.getStartingDate(), typeId, sensorDTO.getUnits());
     }
 }
+
+*/
