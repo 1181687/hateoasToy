bf68095ed882ff3630967df39bb8fc2d523303b5
Joana Figueira <1181710@isep.ipp.pt>
2019-04-15 12:22:54 +0100

    Addresses #154 - Change package of RoomSensorService

--- src/main/java/pt/ipp/isep/dei/project/services/RoomSensorService.java
+++ src/main/java/pt/ipp/isep/dei/project/services/RoomSensorService.java
@@ -1,4 +1,4 @@
-package pt.ipp.isep.dei.project.model.sensor;
+package pt.ipp.isep.dei.project.services;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
