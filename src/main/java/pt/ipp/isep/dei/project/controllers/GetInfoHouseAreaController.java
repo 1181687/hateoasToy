75c6426695e8f43e39f93ffe39b18641416af293
Joana Figueira <1181710@isep.ipp.pt>
2019-04-23 12:54:49 +0100

    Addresses #154 - Started changing UI and Controller of US631

--- src/main/java/pt/ipp/isep/dei/project/controllers/GetInfoHouseAreaController.java
+++ src/main/java/pt/ipp/isep/dei/project/controllers/GetInfoHouseAreaController.java
@@ -5,10 +5,6 @@ import pt.ipp.isep.dei.project.model.house.House;
 import pt.ipp.isep.dei.project.model.sensor.SensorTypeId;
 import pt.ipp.isep.dei.project.services.GeoAreaAggregateService;
 
-import java.time.LocalDate;
-import java.time.LocalDateTime;
-import java.util.HashMap;
-
 
 public class GetInfoHouseAreaController {
     private Location houseLocation;
@@ -59,5 +55,47 @@ public class GetInfoHouseAreaController {
 /*
     public double getAverageDailyRainfall(LocalDate date1, LocalDate date2) {
         return house.getAverageDailyMeasurementInHouseArea(rainfall, date1, date2);
+    }*/
+
+    /**
+     * constructor that receives a House and a SensorType
+     *
+     * @param house parameter
+     */
+    /*public GetFirstHottestDayHouseAreaController(House house) {
+        this.house = house;
+        this.sensorTypeTemperature = new SensorType("temperature");
+    }
+
+    /**
+     * boolean method to check if the sensorlist of a given type is empty or not
+     *
+     * @return boolean - true if it is empty; false if it is not empty (there are sensor of the selected type)
+     */
+    /*public boolean isSensorListOfATypeEmpty() {
+        return house.isSensorListOfAGivenTypeEmpty(sensorTypeTemperature);
+    }
+
+    /**
+     * boolean method that checks if there are readings between selected dates of the chosen sensor (the nearest
+     * one with most recent readings)
+     *
+     * @param startDate Initial of the period the user was to consider
+     * @param endDate   Final of the period the user was to consider
+     * @return boolean - true if there are readings in the interval; false if there are no readings in the interval
+     */
+    /*public boolean checkNearestSensorReadingsExistenceBetweenDates(LocalDate startDate, LocalDate endDate) {
+        return house.checkNearestSensorReadingsExistenceBetweenDates(sensorTypeTemperature, startDate, endDate);
+    }
+
+    /**
+     * method that turns a reading into a readingDTO
+     *
+     * @param startDate Initial of the period the user was to consider
+     * @param endDate   Final of the period the user was to consider
+     * @return ReadingDTO
+     */
+    /*public ReadingDTO getFirstHighestReadingHouseArea(LocalDate startDate, LocalDate endDate) {
+        return ReadingMapper.mapToDTO(house.getFirstHighestReadingHouseArea(sensorTypeTemperature, startDate, endDate));
     }*/
 }
