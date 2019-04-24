720dbb074c4b8bd626f89c2836c734b192a36fb7
luisdealmeida <1181714@isep.ipp.pt>
2019-04-24 15:39:11 +0100

    Addresses #154 - Changes to the methods that correspond to the criteria of choosing a sensor (nearest sensor and fall-back selection).

--- src/main/java/pt/ipp/isep/dei/project/GeoAreaSensorReadingsRepository.java
+++ src/main/java/pt/ipp/isep/dei/project/GeoAreaSensorReadingsRepository.java
@@ -5,13 +5,16 @@ import pt.ipp.isep.dei.project.model.readings.GeoAreaReading;
 import pt.ipp.isep.dei.project.model.readings.GeoAreaReadingId;
 import pt.ipp.isep.dei.project.model.sensor.GeoAreaSensorId;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 public interface GeoAreaSensorReadingsRepository extends CrudRepository<GeoAreaReading, GeoAreaReadingId> {
 
     List<GeoAreaReading> findByGeoAreaReadingId_GeoAreaSensorId(GeoAreaSensorId id);
 
-    /*boolean existsByDateTime_DateBetweenAndGeoAreaReadingId_GeoAreaSensorId(LocalDate startDate, LocalDate endDate, GeoAreaSensorId id);
-
+    boolean existsByGeoAreaReadingId_GeoAreaSensorIdAndGeoAreaReadingId_LocalDateTimeBetween(GeoAreaSensorId sensorId,
+                                                                                             LocalDateTime startDate,
+                                                                                             LocalDateTime endDate);
+/*
     List<GeoAreaReading> findByDateTime_DateBetweenAndGeoAreaReadingId_GeoAreaSensorId(LocalDate startDate, LocalDate endDate, GeoAreaSensorId id);*/
 }
