3c7a458ea7cffb3bc2b1cd674b56ac9246cd6771
Diana Silva <1181702@isep.ipp.pt>
2019-04-24 15:13:25 +0100

    Added tests to AddPowerSourceToHouseGridController

--- src/test/java/pt/ipp/isep/dei/project/controllerTests/AddPowerSourceToHouseGridControllerTest.java
+++ src/test/java/pt/ipp/isep/dei/project/controllerTests/AddPowerSourceToHouseGridControllerTest.java
@@ -10,6 +10,10 @@ import pt.ipp.isep.dei.project.model.house.housegrid.HouseGrid;
 import pt.ipp.isep.dei.project.model.house.housegrid.HouseGridDTO;
 import pt.ipp.isep.dei.project.model.house.housegrid.HouseGridId;
 import pt.ipp.isep.dei.project.model.house.housegrid.HouseGridMapper;
+import pt.ipp.isep.dei.project.model.house.powersource.PowerSourceType;
+import pt.ipp.isep.dei.project.model.house.powersource.PowerSourceTypeDTO;
+import pt.ipp.isep.dei.project.model.house.powersource.PowerSourceTypeId;
+import pt.ipp.isep.dei.project.model.house.powersource.PowerSourceTypeMapper;
 import pt.ipp.isep.dei.project.services.HouseService;
 
 import java.util.ArrayList;
@@ -44,7 +48,6 @@ public class AddPowerSourceToHouseGridControllerTest {
     @Test
     public void isHouseGridRepositoryEmpty_RepositoryIsNotEmpty_ShouldReturnFalse() {
 
-        houseService.addGrid("main");
 
         when(houseService.isGridRepositoryEmpty()).thenReturn(false);
 
@@ -56,7 +59,6 @@ public class AddPowerSourceToHouseGridControllerTest {
 
         //Arrange
         String gridId = "main";
-        houseService.addGrid(gridId);
         String id = "Power Source 1";
         String type = "Battery";
         when(houseService.newPowerSource(id,type,gridId)).thenReturn(true);
@@ -73,7 +75,7 @@ public class AddPowerSourceToHouseGridControllerTest {
 
         //Arrange
         String gridId = "main";
-        houseService.addGrid(gridId);
+
         String id = "Power Source 1";
         String type = "Battery";
         houseService.newPowerSource(id,type,gridId);
@@ -88,11 +90,11 @@ public class AddPowerSourceToHouseGridControllerTest {
 
     @Test
     public void getGridList_ShouldReturnAListOfDTOs() {
-        houseService.addGrid("main");
         HouseGridId id = new HouseGridId("main");
         HouseGrid grid = new HouseGrid(id);
         List<HouseGrid> list =new ArrayList<>();
         list.add(grid);
+
         when(houseService.getAllGrids()).thenReturn(list);
 
         List<HouseGridDTO> dtoList = new ArrayList<>();
@@ -102,6 +104,22 @@ public class AddPowerSourceToHouseGridControllerTest {
         List<HouseGridDTO> result = controller.getGridList();
 
         assertEquals(dtoList.get(0).getId(),result.get(0).getId());
+    }
+
+    @Test
+    public void getPowerSourceTypeListShouldReturnAListOfPowerSourceTypeDTOs(){
+        //Arrange
+        PowerSourceTypeId id = new PowerSourceTypeId("battery");
+        PowerSourceType type = new PowerSourceType(id);
+        List<PowerSourceType> listOfTypes = new ArrayList<>();
+        listOfTypes.add(type);
+        when(houseService.getAllPowerSourceTypes()).thenReturn(listOfTypes);
+        List<PowerSourceTypeDTO> dtoList= new ArrayList<>();
+        dtoList.add(PowerSourceTypeMapper.mapToDTO(type));
+        //Act
+        List<PowerSourceTypeDTO> result = controller.getPowerSourceTypeList();
+        //Assert
+        assertEquals(dtoList.get(0).getType(),result.get(0).getType());
     }
 
 }
