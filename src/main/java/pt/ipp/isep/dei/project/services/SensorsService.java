634c14bc8a59941b54566dd0e381738135f29cd1
Gabriela Alves <1181707@isep.ipp.pt>
2019-04-29 16:04:42 +0100

    Adresses #187 [Related to US445] Creation of method getMapInstantsAboveComfortTemperatureInInterval

--- src/main/java/pt/ipp/isep/dei/project/services/SensorsService.java
+++ src/main/java/pt/ipp/isep/dei/project/services/SensorsService.java
@@ -1,6 +1,11 @@
 package pt.ipp.isep.dei.project.services;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import pt.ipp.isep.dei.project.model.RoomReading;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.*;
 
 public class SensorsService {
 
@@ -10,5 +15,44 @@ public class SensorsService {
     @Autowired
     private RoomSensorService roomSensorService;
 
+    /**
+     * receives a list of readings and gets a Map Map<LocalDateTime, Double>  of instants and temperatures, that were
+     * above the comfort temperature in an interval
+     *
+     * @param mapComfortTemperatureMinMaxByDay Map<LocalDate, List<Double>> with list of the Comfort Temperature Min
+     *                                         and Max for category One or Two or Three, organized By LocalDate
+     * @param intervalRoomReadings             list of Reading in an interval
+     * @return Map<LocalDateTime, Double> map os instants and temperatures above the comfort temperature
+     */
+    public Map<LocalDateTime, Double> getMapInstantsAboveComfortTemperatureInInterval(Map<LocalDate, List<Double>> mapComfortTemperatureMinMaxByDay,
+                                                                                      List<RoomReading> intervalRoomReadings) {
+        Map<LocalDateTime, Double> mapInstantsAboveComfortTemperature = new HashMap<>();
 
+        Set<Map.Entry<LocalDate, List<Double>>> mapComfTemp = mapComfortTemperatureMinMaxByDay.entrySet();
+
+        if (!mapComfTemp.isEmpty()) {
+            for (RoomReading roomReading : intervalRoomReadings) {
+                if (!Objects.isNull(roomReading))
+
+                    for (Map.Entry<LocalDate, List<Double>> mapComfTempDaily : mapComfTemp) {
+
+                        LocalDate localDateReading = roomReading.getDateTime().toLocalDate();
+                        Double valueReading = roomReading.getValue();
+                        LocalDate localDateComfTempMax = mapComfTempDaily.getKey();
+
+                        if (!Objects.isNull(mapComfTempDaily.getValue())) {
+                            Double valueComfTempMax = mapComfTempDaily.getValue().get(1);
+
+                            if (localDateReading.compareTo(localDateComfTempMax) == 0 && Double.compare(valueReading, valueComfTempMax) == 1) {
+                                mapInstantsAboveComfortTemperature.put(roomReading.getDateTime(), valueReading);
+                            }
+                        }
+                    }
+            }
+            return mapInstantsAboveComfortTemperature;
+        }
+        return mapInstantsAboveComfortTemperature;
+    }
+
+    
 }
