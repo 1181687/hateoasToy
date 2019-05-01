fe81745681ece4881ff6945fc069a82447166921
Gabriela Alves <1181707@isep.ipp.pt>
2019-05-01 23:45:01 +0100

    Fixes #220 [Related to US 445]- getReadings in RoomSensorService

--- src/main/java/pt/ipp/isep/dei/project/services/RoomSensorService.java
+++ src/main/java/pt/ipp/isep/dei/project/services/RoomSensorService.java
@@ -2,10 +2,12 @@ package pt.ipp.isep.dei.project.services;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import pt.ipp.isep.dei.project.model.Reading;
 import pt.ipp.isep.dei.project.model.sensor.RoomSensor;
 import pt.ipp.isep.dei.project.model.sensor.SensorId;
 import pt.ipp.isep.dei.project.repositories.RoomSensorRepository;
 
+import java.time.LocalDate;
 import java.util.List;
 
 @Service
@@ -41,4 +43,17 @@ public class RoomSensorService {
     public boolean sensorExists(SensorId sensorId) {
         return this.roomSensorRepo.existsById(sensorId);
     }
+
+    /**
+     * gets room readings from a sensor by sensorId, in an interval of days
+     *
+     * @param startDate initial LocalDate
+     * @param endDate   final LocalDate
+     * @param sensorId  SensorId
+     * @return List of Reading
+     */
+    public List<Reading> getReadings(LocalDate startDate, LocalDate endDate, SensorId sensorId) {
+        return this.roomSensorRepo.findById(sensorId).orElseGet(null).getReadings(startDate, endDate);
+    }
+
 }
