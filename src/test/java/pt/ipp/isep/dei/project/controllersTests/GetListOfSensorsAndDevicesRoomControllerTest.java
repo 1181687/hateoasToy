81d161571b11dff18e3ff3e287bbfc411017758e
Diana Silva <1181702@isep.ipp.pt>
2019-05-03 16:10:26 +0100

    Added tests to GetListOfSensorsAndDevicesRoomController

--- src/test/java/pt/ipp/isep/dei/project/controllersTests/GetListOfSensorsAndDevicesRoomControllerTest.java
+++ src/test/java/pt/ipp/isep/dei/project/controllersTests/GetListOfSensorsAndDevicesRoomControllerTest.java
@@ -1,41 +1,44 @@
-/*
 package pt.ipp.isep.dei.project.controllersTests;
 
+import org.apache.tomcat.jni.Local;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
 import pt.ipp.isep.dei.project.controllers.GetListOfSensorsAndDevicesRoomController;
-import pt.ipp.isep.dei.project.model.Location;
 import pt.ipp.isep.dei.project.model.devices.Device;
-import pt.ipp.isep.dei.project.model.geographicalarea.AreaShape;
-import pt.ipp.isep.dei.project.model.geographicalarea.GeoAreaTypeId;
-import pt.ipp.isep.dei.project.model.geographicalarea.GeographicalArea;
-import pt.ipp.isep.dei.project.model.geographicalarea.GeographicalAreaType;
+
+import pt.ipp.isep.dei.project.model.devices.DeviceType;
+import pt.ipp.isep.dei.project.model.devices.electricwaterheater.ElectricWaterHeater;
+import pt.ipp.isep.dei.project.model.devices.electricwaterheater.ElectricWaterHeaterType;
 import pt.ipp.isep.dei.project.model.house.*;
-import pt.ipp.isep.dei.project.model.sensor.GeoAreaSensor;
-import pt.ipp.isep.dei.project.model.sensor.SensorType;
+import pt.ipp.isep.dei.project.model.sensor.*;
+import pt.ipp.isep.dei.project.services.RoomSensorService;
+import pt.ipp.isep.dei.project.services.RoomService;
 import pt.ipp.isep.dei.project.utils.Utils;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
 
 public class GetListOfSensorsAndDevicesRoomControllerTest {
 
     private GetListOfSensorsAndDevicesRoomController controller;
-    private House house;
-    private RoomList roomList;
     private Room room;
+    @Mock
+    private RoomService roomService;
+    @Mock
+    private RoomSensorService sensorService;
 
     @BeforeEach
     public void StartUp() {
 
-        //Geographical Area
-        Location location = new Location(41.178553, -8.608035, 111);
-        AreaShape areaShape = new AreaShape(0.261, 0.249);
-        GeoAreaTypeId geoAreaTypeId = new GeoAreaTypeId("Urban area");
-        GeographicalAreaType geographicalAreaType = new GeographicalAreaType(geoAreaTypeId);
-        GeographicalArea insertedGeoArea = new GeographicalArea("ISEP", "Campus do ISEP", geographicalAreaType, location, areaShape);
+        MockitoAnnotations.initMocks(this);
 
         // Room 1
         String name = "room1";
@@ -44,234 +47,62 @@ public class GetListOfSensorsAndDevicesRoomControllerTest {
         Dimension dimension = new Dimension(3, 3.5, 3.5);
         this.room = new Room(name, description, houseFloor, dimension);
 
-        //House
-        int meteringPeriodGrid = Integer.parseInt(Utils.readConfigFile("Configuration.properties", "MeteringPeriodGrid"));
-        int meteringPeriodDevice = Integer.parseInt(Utils.readConfigFile("Configuration.properties", "MeteringPeriodDevice"));
-        List<String> deviceTypeList = Utils.readConfigFileToList("Configuration.properties", "devicetype.count", "devicetype.name");
-        this.roomList = house.getRoomList();
-        Location houseLocation = new Location(41.177748, -8.607745, 112);
-        Address address = new Address("4200-072", houseLocation, insertedGeoArea);
-        this.house = new House(deviceTypeList, meteringPeriodGrid, meteringPeriodDevice, address);
 
-
-        this.controller = new GetListOfSensorsAndDevicesRoomController(house);
-    }
-    @Test
-    public void getSensorsListContentOfARoomTest() {
-        // Arrange
-
-        int position = 0;
-        String expectedResult =
-                "1 - Name of the sensor: sensor1\n" +
-                        "2 - Name of the sensor: sensor2\n";
-
-        // Act
-        String result = this.controller.getSensorsListContent(position);
-
-        // Assert
-        assertEquals(expectedResult, result);
+        this.controller = new GetListOfSensorsAndDevicesRoomController(this.roomService, this.sensorService);
     }
 
     @Test
-    public void checkIfSensorListIsEmptyTestTrue() {
-        // Arrange
-        this.house.addRoom(room);
-        int position = 0;
-
-        // Act
-        boolean result = controller.isSensorListEmpty(position);
-
-        // Assert
-        assertTrue(result);
-    }
-
-    @Test
-    public void checkIfSensorListIsEmptyTestFalse() {
-        // Arrange
-        LocalDateTime dataFuncionamento0 = LocalDateTime.of(2015, 11, 2, 15, 20, 00);
-        SensorType sensorType0 = new SensorType("Temperatura");
-        Location locS0 = new Location(123, 345, 50);
-        GeoAreaSensor s0 = new GeoAreaSensor("A123", "sensor1", dataFuncionamento0, sensorType0, locS0, "C");
-
-        room.addSensorToListOfSensorsInRoom(s0);
-        house.addRoom(room);
-        int position = 0;
-
-        // Act
-        boolean result = controller.isSensorListEmpty(position);
-
-        // Assert
-        assertFalse(result);
-    }
-
-
-    @Test
-    public void getDeviceListContentTest() {
-        // Arrange
-        house.createDevice("Fridge", "Fridge1", room);
-
-        house.createDevice("Lamp", "Lamp1", room);
-
-        house.addRoom(room);
-
-        int position = 0;
-        String expectedResult =
-                "1 - Name of the device: Fridge1\n" + "2 - Name of the device: Lamp1\n";
-        // Act
-        String result = controller.getDeviceListContent(position);
-        // Assert
-        assertEquals(expectedResult, result);
-    }
-
-    @Test
-    public void checkIfDeviceListIsEmptyTestTrue() {
-        // Arrange
-        house.addRoom(room);
-        int position = 0;
-        // Act
-        boolean result = controller.isDeviceListEmpty(position);
-        // Assert
-        assertTrue(result);
-    }
-
-    @Test
-    public void checkIfDeviceListIsEmptyTestFalse() {
-        // Arrange
-        Device dev1 = house.createDevice("Lamp", "Lamp1", room);
-
-        house.addRoom(room);
-        int position = 0;
-        // Act
-        boolean result = controller.isDeviceListEmpty(position);
-        // Assert
-        assertFalse(result);
-    }
-
-    @Test
-    public void getListSize() {
-        //arrange
-        String name2 = "Living Room";
-        String description = "room";
-        int houseFloor2 = 1;
-        Dimension dimension2 = new Dimension(2, 1.5, 1.3);
-        Room room2 = new Room(name2, description, houseFloor2, dimension2);
-
-        house.addRoom(room);
-        house.addRoom(room2);
-        int expectResult = 2;
-
-        //act
-        int result = controller.roomListSize();
-
-        //assert
-        assertEquals(expectResult, result);
-    }
-
-    @Test
-    public void getListSizeEmptyList() {
-        //arrange
-        int expectResult = 0;
-
-        //act
-        int result = controller.roomListSize();
-
-        //assert
-        assertEquals(expectResult, result);
-    }
-
-    @Test
-    public void getRoomListTest() {
-        // Arrange
-        house.addRoom(room);
-        RoomList expectedResult = this.roomList;
-
-        // Act
-        RoomList result = this.controller.getListOfRooms();
-
-        // Assert
-        assertEquals(expectedResult, result);
-    }
-
-    @Test
-    public void getRoomListEmptyTest() {
-        //arrange
-        RoomList expectResult = this.roomList;
-
-        //act
-        RoomList result = this.controller.getListOfRooms();
-
-        //assert
-        assertEquals(expectResult, result);
-    }
-
-    @Test
-    public void testGetNameOfRoomInListOfRooms() {
+    public void getRoomDTOList() {
         //Arrange
-        Dimension dim1 = new Dimension(4, 4, 4);
-        Room room1 = new Room("RoomTwo", "room", 1, dim1);
-        this.roomList.addRoom(room);
-        this.roomList.addRoom(room1);
+        List<Room> rooms = new ArrayList<>();
+        rooms.add(this.room);
 
-        String expectedResult = "RoomTwo";
-        int roomPos = 1;
+        when(this.roomService.getAllRooms()).thenReturn(rooms);
+
+        List<RoomDTO> expectedResult = new ArrayList<>();
+        expectedResult.add(RoomMapper.mapToDTO(this.room));
 
         //Act
-        String result = this.controller.getRoomNameByPosition(roomPos);
-
+        List<RoomDTO> result = this.controller.getRoomDTOList();
         //Assert
-        assertEquals(expectedResult, result);
+        assertEquals(expectedResult,result);
     }
 
     @Test
-    public void testGetNameOfRoomInEmptyListOfRooms() {
+    public void getRoomSensorDTOList(){
         //Arrange
-        String expectedResult = null;
-        int roomPos = 0;
+        RoomId roomId = new RoomId(this.room.getRoomId());
+        LocalDateTime startingDate = LocalDateTime.of(2018,11,14,23,50);
+        SensorId sensorId = new SensorId("TT1234");
+        SensorTypeId typeId = new SensorTypeId("Temperature");
+        RoomSensor sensor = new RoomSensor(sensorId,"TT12345",startingDate,typeId,"C",roomId);
+
+        LocalDateTime startingDate2 = LocalDateTime.of(2017,1,14,23,50);
+        SensorId sensorId2 = new SensorId("H1234");
+        SensorTypeId typeId2 = new SensorTypeId("Humidity");
+        RoomSensor sensor2 = new RoomSensor(sensorId2,"TT12345",startingDate2,typeId2,"l/m3",roomId);
+
+        List<RoomSensor> sensors = Arrays.asList(sensor,sensor2);
+
+        when(this.sensorService.getAllSensorsOfRoom(roomId)).thenReturn(sensors);
+
+        List<RoomSensorDTO> expectedResult = Arrays.asList(RoomSensorMapper.mapToDTO(sensor),RoomSensorMapper.mapToDTO(sensor2));
 
         //Act
-        String result = this.controller.getRoomNameByPosition(roomPos);
-
+        List<RoomSensorDTO> result = this.controller.getRoomSensorDTOList(this.room.getRoomId());
         //Assert
-        assertEquals(expectedResult, result);
+        assertEquals(expectedResult,result);
+
     }
 
     @Test
-    public void getDisplayRoomListTest() {
-        //arrange
-        String name1 = "Kitchen";
-        String description = "room";
-        int houseFloor1 = 0;
-        Dimension dimension1 = new Dimension(2, 2, 2);
-        Room room1 = new Room(name1, description, houseFloor1, dimension1);
+    public void getDeviceDTOList(){
+        DeviceType type = new ElectricWaterHeaterType();
+        Device electricWaterHeater = type.createDevice("ewh2000");
+        electricWaterHeater.setLocation(this.room);
+        electricWaterHeater.setAttributesDevType("Hot-Water Temperature", 55);
+        electricWaterHeater.setAttributesDevType("Performance Ratio", 0.9);
+        electricWaterHeater.setAttributesDevType("Nominal Power", 700);
 
-        String name2 = "Living Room";
-        int houseFloor2 = 1;
-        Dimension dimension2 = new Dimension(2, 1.5, 1.3);
-        Room room2 = new Room(name2, description, houseFloor2, dimension2);
-
-        this.house.addRoom(room1);
-        this.house.addRoom(room2);
-
-        String expectResult = "1- Name: Kitchen, House Floor: 0, Dimension - Height: 2.0, Length: 2.0, Width: 2.0\n" +
-                "2- Name: Living Room, House Floor: 1, Dimension - Height: 2.0, Length: 1.5, Width: 1.3\n";
-
-        //act
-        String result = this.controller.getRoomListContent();
-
-        //assert
-        assertEquals(expectResult, result);
-    }
-
-    @Test
-    public void getDisplayRoomListEmptyTest() {
-        //arrange
-        String expectResult = "";
-
-        //act
-        String result = this.controller.getRoomListContent();
-
-        //assert
-        assertEquals(expectResult, result);
     }
 }
-*/
